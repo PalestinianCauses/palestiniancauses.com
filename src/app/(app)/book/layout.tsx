98265fdb771b154c06a4295caad3ad7d1eb91509
Shawqi Hatem <shawqicauses@outlook.com>
2025-01-05 03:26:15 +0200

    Update: Enhance through removing authors names

--- src/app/(app)/book/layout.tsx
+++ src/app/(app)/book/layout.tsx
@@ -1,11 +1,11 @@
-// REVIEWED
+// REVIEWED - 01
 import type { Metadata } from "next";
 import { PropsWithChildren } from "react";
 
 export const metadata: Metadata = {
   title: "A Human But From Gaza: PalestinianCauses' Book",
   description:
-    '"A Human But From Gaza" shares the powerful diaries of Maha and Lama Matar, revealing life during the ongoing war in Gaza. Enhanced with the artwork of Nour Matar, these three sisters and the PalestinianCauses team capture the resilience, loss, and hope of those fighting to be seen and remembered.',
+    '"A Human But From Gaza" shares the powerful diaries of M. and L., revealing life during the ongoing war in Gaza. Enhanced with the artwork of N., these three sisters and the PalestinianCauses team capture the resilience, loss, and hope of those fighting to be seen and remembered.',
 };
 
 export default function BookLayout({ children }: PropsWithChildren) {
