5c615a635cbd6a0d20da83ec0d083bddeea708b0
Shawqi Hatem <shawqicauses@outlook.com>
2025-01-12 18:52:31 +0200

    Update: Enhance through updating SEO in website pages

--- src/app/(app)/book/layout.tsx
+++ src/app/(app)/book/layout.tsx
@@ -1,9 +1,9 @@
-// REVIEWED - 01
+// REVIEWED - 02
 import type { Metadata } from "next";
 import { PropsWithChildren } from "react";
 
 export const metadata: Metadata = {
-  title: "A Human But From Gaza: PalestinianCauses' Book",
+  title: "A Human But From Gaza",
   description:
     '"A Human But From Gaza" shares the powerful diaries of M. and L., revealing life during the ongoing war in Gaza. Enhanced with the artwork of N., these three sisters and the PalestinianCauses team capture the resilience, loss, and hope of those fighting to be seen and remembered.',
 };
