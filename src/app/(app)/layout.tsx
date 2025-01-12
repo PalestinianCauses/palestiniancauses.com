42f8e5fdba972669674b96c10b4d81066d8ad9a7
Shawqi Hatem <shawqicauses@outlook.com>
2025-01-12 18:52:31 +0200

    Update: Enhance through updating SEO in website pages

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,11 +1,12 @@
-// REVIEWED - 06
+// REVIEWED - 07
 import type { Metadata } from "next";
 import { PropsWithChildren } from "react";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Home - PalestinianCauses",
-  description: "No description available for now.",
+  title: { template: "%s | PalestinianCauses", default: "PalestinianCauses" },
+  description:
+    "PalestinianCauses LLC is a U.S.-registered company that creates innovative digital solutions and e-commerce experiences driven by Palestinians with specialized expertise and a unique perspective. Our platform is structured around 'rooms,' each offering exceptional content, services, or products. Our current project features the book A Human But From Gaza, and we have long-term plans to develop team members' rooms to showcase their digital solutions and tech-based services. We aim to foster meaningful connections and global awareness through storytelling, creativity, and technology.",
 };
 
 const RootLayout = function RootLayout({ children }: PropsWithChildren) {
