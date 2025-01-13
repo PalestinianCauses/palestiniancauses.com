790e72e14566ee3d9294c7ec8f52df03ffbf7eb8
Shawqi Hatem <shawqicauses@outlook.com>
2025-01-13 20:55:15 +0200

    New: Enhance through adding cart actions

---
+++ src/actions/cart.ts
@@ -0,0 +1,45 @@
+"use server";
+
+// REVIEWED
+
+/* eslint-disable consistent-return */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { revalidateTag } from "next/cache";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { TAGS } from "@/lib/constants";
+import { createCart, getCart, insertToCart } from "@/lib/shopify";
+
+export const createCartPlusSetCookie =
+  async function createCartPlusSetCookie() {
+    const cart = await createCart();
+    (await cookies()).set("cartId", cart.id!);
+  };
+
+export const insertItem = async function insertItem(
+  previousState: any,
+  selectedVariantId: string | undefined,
+) {
+  const cartId = (await cookies()).get("cartId")?.value;
+
+  if (!cartId || !selectedVariantId) return "Error adding item to cart.";
+
+  try {
+    await insertToCart(cartId, [
+      { merchandiseId: selectedVariantId, quantity: 1 },
+    ]);
+
+    revalidateTag(TAGS.cart);
+    /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+  } catch (error) {
+    return "Error adding item to cart.";
+  }
+};
+
+export const redirectToCheckout = async function redirectToCheckout() {
+  const cartId = (await cookies()).get("cartId")?.value;
+  const cart = await getCart(cartId);
+  redirect(cart!.checkoutUrl);
+};
