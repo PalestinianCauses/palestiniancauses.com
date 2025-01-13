29e2e9bcb3a377bfa65592d7136e01a7d0bd2e68
Shawqi Hatem <shawqicauses@outlook.com>
2025-01-13 02:00:20 +0200

    New: Enhance through adding `ensureStartsWith` utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,16 @@
-// REVIEWED
+// REVIEWED - 01
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export const cn = function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+};
+
+export const ensureStartsWith = function ensureStartsWith(
+  stringToCheck: string,
+  startsWith: string,
+) {
+  return stringToCheck.startsWith(startsWith)
+    ? stringToCheck
+    : [startsWith, stringToCheck].join("");
 };
