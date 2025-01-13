4135a6f04878357dbc6717f3b437f1c029ce930a
Shawqi Hatem <shawqicauses@outlook.com>
2025-01-13 02:07:21 +0200

    New: Enhance through adding some constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,10 @@
+// REVIEWED
+export const SHOPIFY_GRAPHQL_API_ENDPOINT = "/api/2025-01/graphql.json";
+
+export const HIDDEN_PRODUCT_TAG = "nextjs-front-end-hidden";
+
+export const TAGS = {
+  collections: "collections",
+  products: "products",
+  cart: "cart",
+};
