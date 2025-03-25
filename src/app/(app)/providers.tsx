be86ad7b0bb10ffad7dde47ff31e42bd9069599b
Shawqi Hatem <shawqicauses@outlook.com>
2025-03-25 09:32:50 +0200

    Fix: Enhance through fixing `Providers` component

--- src/app/(app)/providers.tsx
+++ src/app/(app)/providers.tsx
@@ -1,14 +1,9 @@
 "use client";
 
-// REVIEWED
+// REVIEWED - 01
 
-import { FrappeProvider } from "frappe-react-sdk";
 import { PropsWithChildren } from "react";
 
 export const Providers = function Providers({ children }: PropsWithChildren) {
-  return (
-    <FrappeProvider url={process.env.NEXT_PUBLIC_FRAPPE_URL}>
-      {children}
-    </FrappeProvider>
-  );
+  return children;
 };
