00f3f2efb5ee7018ae56a3eb3aa502198f7b4c17
Shawqi Hatem <shawqicauses@outlook.com>
2025-03-25 11:46:38 +0200

    New: Enhance through adding `populateUser` hook to `DiaryEntries` collection

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,12 +1,63 @@
-// REVIEWED
+// REVIEWED - 01
 import type { CollectionConfig } from "payload";
+
+import { syncUserWithFrappe } from "@/lib/payload/hooks/user";
 
 export const Users: CollectionConfig = {
   slug: "users",
   admin: { useAsTitle: "email" },
   auth: true,
   fields: [
-    // Email added by default
-    // Add more fields as needed
+    {
+      label: "Email",
+      name: "email",
+      type: "email",
+      required: true,
+      unique: true,
+    },
+    {
+      label: "First Name",
+      name: "firstName",
+      type: "text",
+      required: true,
+    },
+    {
+      label: "Last Name",
+      name: "lastName",
+      type: "text",
+      required: true,
+    },
+    {
+      admin: { hidden: true },
+      label: "Role",
+      name: "role",
+      type: "text",
+      required: true,
+    },
+    {
+      admin: { hidden: true },
+      label: "Frappe User ID",
+      name: "frappeUserId",
+      type: "text",
+      required: true,
+      unique: true,
+    },
+    {
+      admin: { hidden: true },
+      label: "Frappe User Role",
+      name: "frappeUserRole",
+      type: "text",
+      required: true,
+    },
+    {
+      admin: { hidden: true },
+      label: "Is Synced With Frappe?",
+      name: "isSyncedWithFrappe",
+      type: "checkbox",
+      defaultValue: false,
+    },
   ],
+  hooks: {
+    afterChange: [syncUserWithFrappe],
+  },
 };
