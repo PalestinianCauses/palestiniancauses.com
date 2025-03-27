b05b9eb4bf526025b9377fa37e60b5aa2b3f2129
Shawqi Hatem <shawqicauses@outlook.com>
2025-03-27 20:25:29 +0200

    Update: Enhance through adding logo to `Navbar` component

--- src/components/globals/navbar.tsx
+++ src/components/globals/navbar.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-// REVIEWED - 01
+// REVIEWED - 02
 
+import Image from "next/image";
 import Link from "next/link";
 
 import { Button } from "../ui/button";
@@ -12,10 +13,25 @@ export const Navbar = function Navbar() {
   return (
     <nav
       aria-label="Global"
-      className="fixed left-0 right-0 top-0 z-10 border-b border-foreground/10 bg-foreground/5 py-5 backdrop-blur-md">
+      className="fixed left-0 right-0 top-0 z-20 border-b border-foreground/10 py-5 backdrop-blur">
       <Container>
-        <div className="flex items-center justify-between">
-          <ul className="ml-auto flex items-center gap-5">
+        <div className="xs:flex-row flex flex-col items-center justify-between gap-5">
+          <Link href="/book" className="flex items-center gap-5">
+            <Image
+              src="/pc-logo-primary-foreground.png"
+              alt="PC Logo"
+              sizes="3rem"
+              fill
+              className="!static !h-auto !w-12"
+            />
+            <Button
+              variant="link"
+              className="h-auto px-0 py-0 text-base leading-none"
+              asChild>
+              <span>PalestinianCauses.</span>
+            </Button>
+          </Link>
+          <ul className="xs:ml-auto flex items-center gap-3">
             <li>
               <Button variant="ghost" className="font-semibold" asChild>
                 <Link href="/signup">Sign up</Link>
