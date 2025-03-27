bf97fa99e36a292bb7de23d9d107c034dbb285ad
Shawqi Hatem <shawqicauses@outlook.com>
2025-03-27 16:37:43 +0200

    New: Enhance through adding `AuthLayout` component

---
+++ src/app/(app)/(auth)/layout.tsx
@@ -0,0 +1,27 @@
+// REVIEWED
+
+import Image from "next/image";
+import { PropsWithChildren } from "react";
+
+import { HeroBackgroundPattern } from "@/components/book/hero-background-pattern";
+import { Container } from "@/components/globals/container";
+
+export default function AuthLayout({ children }: PropsWithChildren) {
+  return (
+    <div className="my-40">
+      <HeroBackgroundPattern />
+      <Container className="max-w-lg">
+        <Image
+          src="/pc-logo-primary-foreground.png"
+          alt="PalestinianCauses Logo"
+          sizes="3rem"
+          fill
+          className="!static mx-auto mb-10 !h-auto !w-24 object-cover"
+        />
+        <div className="flex flex-col items-stretch justify-center">
+          {children}
+        </div>
+      </Container>
+    </div>
+  );
+}
