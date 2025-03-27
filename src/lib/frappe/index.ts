546c438a4867b03105bb4db13d5e1e1f2345769c
Shawqi Hatem <shawqicauses@outlook.com>
2025-03-27 20:56:07 +0200

    New: Enhance through refactoring `lib/frappe/index.ts` file

--- src/lib/frappe/index.ts
+++ src/lib/frappe/index.ts
@@ -1,8 +1,18 @@
-// REVIEWED
+// REVIEWED - 01
 
 import { FrappeApp } from "frappe-js-sdk";
 
-const frappe = new FrappeApp(process.env.NEXT_PUBLIC_FRAPPE_URL!);
+const frappe = new FrappeApp(process.env.NEXT_PUBLIC_FRAPPE_URL!, {
+  useToken: true,
+  token: () =>
+    [process.env.FRAPPE_API_KEY!, process.env.FRAPPE_PUBLIC_API_SECRET!].join(
+      ":",
+    ),
+  type: "token",
+});
+
+export const frappeDB = frappe.db();
+export const frappeCall = frappe.call();
+export const frappeAxios = frappe.axios({});
 
 export const frappeAuthentication = frappe.auth();
-export const frappeDB = frappe.db();
