e13f9503afe1b162734afcbc685d820c6fffcbc9
Shawqi Hatem <shawqicauses@outlook.com>
2025-03-27 16:43:54 +0200

    Fix: Enhance through fixing semantic HTML in `Navbar` component

--- src/components/globals/navbar.tsx
+++ src/components/globals/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-// REVIEWED
+// REVIEWED - 01
 
 import Link from "next/link";
 
@@ -10,9 +10,11 @@ import { Container } from "./container";
 
 export const Navbar = function Navbar() {
   return (
-    <header className="fixed left-0 right-0 top-0 z-10 border-b border-foreground/5 bg-foreground/[2.5%] py-5 backdrop-blur-md">
+    <nav
+      aria-label="Global"
+      className="fixed left-0 right-0 top-0 z-10 border-b border-foreground/10 bg-foreground/5 py-5 backdrop-blur-md">
       <Container>
-        <nav aria-label="Global" className="flex items-center justify-between">
+        <div className="flex items-center justify-between">
           <ul className="ml-auto flex items-center gap-5">
             <li>
               <Button variant="ghost" className="font-semibold" asChild>
@@ -25,8 +27,8 @@ export const Navbar = function Navbar() {
               </Button>
             </li>
           </ul>
-        </nav>
+        </div>
       </Container>
-    </header>
+    </nav>
   );
 };
