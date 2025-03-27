9b0f8bf4a95f4d6fd423c26530f3c68c342e65b9
Shawqi Hatem <shawqicauses@outlook.com>
2025-03-27 16:38:46 +0200

    Fix: Enhance through fixing `ref` forwarding in `Container` component

--- src/components/globals/container.tsx
+++ src/components/globals/container.tsx
@@ -1,4 +1,4 @@
-// REVIEWED - 01
+// REVIEWED - 02
 
 import { forwardRef, HTMLAttributes } from "react";
 
@@ -7,8 +7,8 @@ import { cn } from "@/lib/utils";
 export const Container = forwardRef<
   HTMLDivElement,
   HTMLAttributes<HTMLDivElement>
->(({ className, children }) => (
-  <div className={cn("mx-auto max-w-7xl px-6 lg:px-8", className)}>
+>(({ className, children }, ref) => (
+  <div ref={ref} className={cn("mx-auto max-w-7xl px-6 lg:px-8", className)}>
     {children}
   </div>
 ));
