0c17a6fab16b5f6091ae3a7eb977f29668c0e5a3
Shawqi Hatem <shawqicauses@outlook.com>
2025-03-28 19:34:22 +0200

    New: Enhance through adding `isErrorPayload` to `lib/payload/index.ts` file

---
+++ src/lib/payload/index.ts
@@ -0,0 +1,30 @@
+// REVIEWED
+
+import { getPayload } from "payload";
+
+import { User } from "@/payload-types";
+import configPromise from "@payload-config";
+
+export const payload = await getPayload({ config: configPromise });
+
+export type ErrorPayload = {
+  status: number;
+  errors: Array<{ message: string }>;
+};
+
+export type SignInResponsePayload = {
+  token: string | undefined;
+  user: User | undefined;
+};
+
+export const isErrorPayload = function isErrorPayload(
+  error: unknown,
+): error is ErrorPayload {
+  return (
+    error !== null &&
+    typeof error === "object" &&
+    "status" in error &&
+    "errors" in error &&
+    Array.isArray((error as ErrorPayload).errors)
+  );
+};
