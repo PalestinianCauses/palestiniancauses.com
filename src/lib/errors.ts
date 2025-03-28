b58920413c51a38a85837d79ba2f5853005c003c
Shawqi Hatem <shawqicauses@outlook.com>
2025-03-28 18:53:37 +0200

    New: Enhance through adding `httpStatusesMessages` to `lib/errors.ts` file

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,5 +1,25 @@
-// REVIEWED
+// REVIEWED - 01
+
 export const messages = {
+  actions: {
+    auth: {
+      signIn: {
+        notFound(email: string) {
+          return ["User with", email, "email does not exist."].join(" ");
+        },
+        unAuthorized(email: string) {
+          return [
+            "Password entered for user with",
+            email,
+            "email is not correct.",
+          ].join(" ");
+        },
+        serverError() {
+          return ["An error occurred while signing in."].join(" ");
+        },
+      },
+    },
+  },
   forms: {
     required(field: string) {
       return ["Please enter your", field].join(" ");
@@ -7,5 +27,21 @@ export const messages = {
     valid(field: string) {
       return ["Please enter a valid", field].join(" ");
     },
+  },
+};
+
+export const httpStatusesMessages = {
+  401: {
+    signIn(email: string) {
+      return messages.actions.auth.signIn.unAuthorized(email);
+    },
+  },
+  404: {
+    signIn(email: string) {
+      return messages.actions.auth.signIn.notFound(email);
+    },
+  },
+  500: {
+    signIn: messages.actions.auth.signIn.serverError(),
   },
 };
