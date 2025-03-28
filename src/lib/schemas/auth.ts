a861d116e0c62ef919b4055850b2eb7ec3b5877d
Shawqi Hatem <shawqicauses@outlook.com>
2025-03-29 01:30:03 +0200

    New: Enhance through adding `signUpSchema` to `lib/schemas/auth.ts` file

--- src/lib/schemas/auth.ts
+++ src/lib/schemas/auth.ts
@@ -1,4 +1,4 @@
-// REVIEWED
+// REVIEWED - 01
 
 import { z } from "zod";
 
@@ -12,4 +12,15 @@ export const signInSchema = z.object({
   password: z.string().min(2, messages.forms.required("password")),
 });
 
+export const signUpSchema = z.object({
+  firstName: z.string().min(2, messages.forms.required("first name")),
+  lastName: z.string().min(2, messages.forms.required("last name")),
+  email: z
+    .string()
+    .email(messages.forms.required("email"))
+    .min(2, messages.forms.required("email")),
+  password: z.string().min(2, messages.forms.required("password")),
+});
+
 export type SignInSchema = z.infer<typeof signInSchema>;
+export type SignUpSchema = z.infer<typeof signUpSchema>;
