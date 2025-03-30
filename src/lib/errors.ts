1871cca038f847795d1d525847cad65f17073264
Shawqi Hatem <shawqicauses@outlook.com>
2025-03-30 02:46:35 +0200

    Update: Enhance through adding sign up error messages to `lib/errors.ts` file

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,4 +1,4 @@
-// REVIEWED - 02
+// REVIEWED - 03
 
 export const messages = {
   actions: {
@@ -6,9 +6,6 @@ export const messages = {
       signIn: {
         pending: "Signing in...",
         success: "Signed in successfully.",
-        notFound(email: string) {
-          return ["User with", email, "email does not exist."].join(" ");
-        },
         unAuthorized(email: string) {
           return [
             "Password entered for user with",
@@ -16,9 +13,40 @@ export const messages = {
             "email is not correct.",
           ].join(" ");
         },
+        notFound(email: string) {
+          return [
+            "User with",
+            email,
+            "is not yet a family member.",
+            "May be sign up first?",
+          ].join(" ");
+        },
         serverError() {
           return [
             "An error occurred while signing in.",
+            "Please try again later.",
+          ].join(" ");
+        },
+      },
+      signUp: {
+        pending: "Signing up...",
+        success: "Signed up successfully.",
+        duplication(email: string) {
+          return [
+            "User with",
+            email,
+            "email is already a family member.",
+            "May be sign in instead?",
+          ].join(" ");
+        },
+        validationError() {
+          return [
+            "Password entered is not as resilient as Gaza's people.",
+          ].join(" ");
+        },
+        serverError() {
+          return [
+            "An error occurred while signing up.",
             "Please try again later.",
           ].join(" ");
         },
@@ -46,7 +74,16 @@ export const httpStatusesMessages = {
       return messages.actions.auth.signIn.notFound(email);
     },
   },
+  409: {
+    signUp(email: string) {
+      return messages.actions.auth.signUp.duplication(email);
+    },
+  },
+  417: {
+    signUp: messages.actions.auth.signUp.validationError(),
+  },
   500: {
     signIn: messages.actions.auth.signIn.serverError(),
+    signUp: messages.actions.auth.signUp.serverError(),
   },
 };
