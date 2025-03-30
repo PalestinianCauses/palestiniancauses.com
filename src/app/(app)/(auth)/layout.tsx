4754debbc9b066b900b3948d14b0f7ccccfed251
Shawqi Hatem <shawqicauses@outlook.com>
2025-03-30 02:58:26 +0200

    Update: Enhance through updating `AuthLayout` component style

--- src/app/(app)/(auth)/layout.tsx
+++ src/app/(app)/(auth)/layout.tsx
@@ -1,22 +1,20 @@
-// REVIEWED - 01
+// REVIEWED - 02
 
 import Image from "next/image";
 import { PropsWithChildren } from "react";
 
-import { HeroBackgroundPattern } from "@/components/book/hero-background-pattern";
 import { Container } from "@/components/globals/container";
 
 export default function AuthLayout({ children }: PropsWithChildren) {
   return (
-    <div className="xs:my-40 my-48">
-      <HeroBackgroundPattern />
+    <div className="my-48 xs:my-40">
       <Container className="max-w-lg">
         <Image
           src="/pc-logo-primary-foreground.png"
           alt="PalestinianCauses Logo"
           sizes="3rem"
           fill
-          className="!static mx-auto mb-10 !h-auto !w-24 object-cover"
+          className="!static mx-auto mb-8 !h-auto !w-24 object-cover"
         />
         <div className="flex flex-col items-stretch justify-center">
           {children}
