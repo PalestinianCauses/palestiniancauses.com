fa5ee1664259dac24414a9dae6db09362aea2931
Shawqi Hatem <shawqicauses@outlook.com>
2025-03-30 03:12:32 +0200

    New: Enhance through adding `SignUpPage` component

---
+++ src/app/(app)/(auth)/signup/page.tsx
@@ -0,0 +1,28 @@
+// REVIEWED
+
+import { Metadata } from "next";
+import { Fragment } from "react";
+
+import { SignUpForm } from "@/components/auth/forms/sign-up";
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+};
+
+export default function SignInPage() {
+  return (
+    <Fragment>
+      <div className="mb-10 flex flex-col items-center justify-center gap-3">
+        <h2 className="text-center text-2xl font-semibold leading-none">
+          Welcome at PalestinianCauses.
+        </h2>
+        <p className="text-center text-sm leading-normal text-muted-foreground">
+          Let us be united together to share{" "}
+          <span className="font-medium text-foreground">Gaza&apos;s</span>{" "}
+          voices.
+        </p>
+      </div>
+      <SignUpForm />
+    </Fragment>
+  );
+}
