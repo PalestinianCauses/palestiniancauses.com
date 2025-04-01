1732d068e9f5c8634f4909eb81612d894bc9a3ac
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-01 18:21:25 +0200

    Update: Enhance through updating `AuthLayout` component

--- src/app/(app)/(auth)/layout.tsx
+++ src/app/(app)/(auth)/layout.tsx
@@ -1,25 +1,28 @@
-// REVIEWED - 02
+// REVIEWED - 03
 
 import Image from "next/image";
 import { PropsWithChildren } from "react";
 
+import { Public } from "@/components/auth/public";
 import { Container } from "@/components/globals/container";
 
 export default function AuthLayout({ children }: PropsWithChildren) {
   return (
-    <div className="my-48 xs:my-40">
-      <Container className="max-w-lg">
-        <Image
-          src="/pc-logo-primary-foreground.png"
-          alt="PalestinianCauses Logo"
-          sizes="3rem"
-          fill
-          className="!static mx-auto mb-8 !h-auto !w-24 object-cover"
-        />
-        <div className="flex flex-col items-stretch justify-center">
-          {children}
-        </div>
-      </Container>
-    </div>
+    <Public>
+      <div className="my-48 xs:my-40">
+        <Container className="max-w-lg">
+          <Image
+            src="/pc-logo-primary-foreground.png"
+            alt="PalestinianCauses Logo"
+            sizes="3rem"
+            fill
+            className="!static mx-auto mb-8 !h-auto !w-24 object-cover"
+          />
+          <div className="flex flex-col items-stretch justify-center">
+            {children}
+          </div>
+        </Container>
+      </div>
+    </Public>
   );
 }
