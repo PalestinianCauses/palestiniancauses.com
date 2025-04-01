1a5fa50d2ba2115119d1c59e27153f499fec93e5
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-01 17:58:49 +0200

    New: Enhance through adding `signOut` to `Navbar` component

--- src/components/globals/navbar.tsx
+++ src/components/globals/navbar.tsx
@@ -1,21 +1,26 @@
 "use client";
 
-// REVIEWED - 02
+// REVIEWED - 03
 
 import Image from "next/image";
 import Link from "next/link";
+import { Fragment } from "react";
+
+import { useUser } from "@/hooks/use-user";
 
 import { Button } from "../ui/button";
 
 import { Container } from "./container";
 
 export const Navbar = function Navbar() {
+  const { isPending, signOut, user } = useUser();
+
   return (
     <nav
       aria-label="Global"
       className="fixed left-0 right-0 top-0 z-20 border-b border-foreground/10 py-5 backdrop-blur">
       <Container>
-        <div className="xs:flex-row flex flex-col items-center justify-between gap-5">
+        <div className="flex flex-col items-center justify-between gap-5 xs:flex-row">
           <Link href="/book" className="flex items-center gap-5">
             <Image
               src="/pc-logo-primary-foreground.png"
@@ -31,17 +36,35 @@ export const Navbar = function Navbar() {
               <span>PalestinianCauses.</span>
             </Button>
           </Link>
-          <ul className="xs:ml-auto flex items-center gap-3">
-            <li>
-              <Button variant="ghost" className="font-semibold" asChild>
-                <Link href="/signup">Sign up</Link>
-              </Button>
-            </li>
-            <li>
-              <Button className="font-semibold" asChild>
-                <Link href="/signin">Sign in</Link>
-              </Button>
-            </li>
+          <ul className="flex items-center gap-3 xs:ml-auto">
+            {(!isPending && !user && (
+              <Fragment>
+                <li>
+                  <Button variant="ghost" className="font-semibold" asChild>
+                    <Link href="/signup">Sign up</Link>
+                  </Button>
+                </li>
+                <li>
+                  <Button className="font-semibold" asChild>
+                    <Link href="/signin">Sign in</Link>
+                  </Button>
+                </li>
+              </Fragment>
+            )) ||
+              (!isPending && user && (
+                <li>
+                  <Button
+                    variant="ghost"
+                    disabled={signOut.isPending}
+                    className="font-semibold"
+                    onClick={() => {
+                      signOut.mutate();
+                    }}>
+                    {signOut.isPending ? "Signing out..." : "Sign out"}
+                  </Button>
+                </li>
+              )) ||
+              null}
           </ul>
         </div>
       </Container>
