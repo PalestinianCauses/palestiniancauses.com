3bc1f9d8520ef79c7c5ba254bbac2570781283de
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-01 18:23:47 +0200

    New: Enhance through adding `SharePage`

---
+++ src/app/(app)/humans-but-from-gaza/share/page.tsx
@@ -0,0 +1,38 @@
+// REVIEWED
+
+import { Metadata } from "next";
+
+import { Protected } from "@/components/auth/protected";
+import { CreateDiaryEntryForm } from "@/components/diary-entry/forms/create-diary-entry";
+import { Container } from "@/components/globals/container";
+
+export const metadata: Metadata = {
+  title: "Share Your Diary With The Truth Museum.",
+  description:
+    "Your voice matters. Share your diary entries from Gaza's war with The Truth Museum and help us preserve and amplify these crucial stories.",
+};
+
+export default function SharePage() {
+  return (
+    <Protected>
+      <main className="flex flex-col items-center justify-center py-48 xs:py-40">
+        <Container>
+          <div className="mb-10 flex flex-col items-center justify-center gap-3">
+            <h1
+              className="font-stretch mt-10 max-w-3xl truncate text-pretty bg-gradient-to-b from-foreground/25 via-foreground to-foreground/50 bg-cover bg-clip-text bg-center bg-no-repeat text-center text-5xl font-bold tracking-tight text-foreground sm:text-7xl"
+              style={{ WebkitTextFillColor: "transparent" }}>
+              Share Your Diary With The Truth Museum.
+            </h1>
+            <p className="mx-auto max-w-4xl text-center leading-normal text-muted-foreground">
+              We understand the weight of sharing experiences from Gaza&apos;s
+              war. Like you, our team has witnessed the same heartache. Join us
+              in amplifying Gaza&apos;s stories and ensuring every voice is
+              heard. Together, we can make a difference.
+            </p>
+          </div>
+          <CreateDiaryEntryForm />
+        </Container>
+      </main>
+    </Protected>
+  );
+}
