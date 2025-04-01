a92076dcc64cc6a64e6f2a5fa8f7aa6dc450de48
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-01 18:02:45 +0200

    New: Enhance through adding `queryClient` to `providers.tsx` file

--- src/app/(app)/providers.tsx
+++ src/app/(app)/providers.tsx
@@ -1,9 +1,25 @@
 "use client";
 
-// REVIEWED - 01
+// REVIEWED - 02
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PropsWithChildren } from "react";
 
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 60 * 1000,
+      refetchOnWindowFocus: true,
+    },
+  },
+});
+
 export const Providers = function Providers({ children }: PropsWithChildren) {
-  return children;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 };
