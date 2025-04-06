4e4b110903e2daea7250886bfe971b76a4c8c1f7
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-07 00:00:10 +0200

    Update: Enhance through updating `/` home page content

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,9 +1,15 @@
-// REVIEWED - 01
+// REVIEWED - 02
 
-import { redirect } from "next/navigation";
+import { Intro } from "@/components/globals/intro";
+import { Navigation } from "@/components/globals/navigation";
 
-const HomePage = function HomePage() {
-  redirect("/book");
+const HomePage = async function HomePage() {
+  return (
+    <main className="grid h-screen max-h-screen min-h-[48rem] snap-y snap-mandatory grid-cols-1 divide-y divide-muted overflow-y-scroll lg:snap-none lg:grid-cols-2 lg:divide-x lg:divide-y-0 [&_>_*]:h-screen [&_>_*]:max-h-screen [&_>_*]:min-h-[48rem]">
+      <Intro />
+      <Navigation />
+    </main>
+  );
 };
 
 export default HomePage;
