1af247c9e4bbd7047e7324086e7b746b11f352f7
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-09 17:21:52 +0200

    New: Enhance through adding `globals/motion.tsx` file

---
+++ src/components/globals/motion.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+// REVIEWED
+
+import { motion } from "motion/react";
+
+export const MotionDiv = motion.div;
+
+export const MotionH1 = motion.h1;
+export const MotionH2 = motion.h2;
+
+export const MotionP = motion.p;
+
+export const MotionA = motion.a;
+export const MotionButton = motion.button;
+
+export const MotionSpan = motion.span;
