e4ac086c6b337b9e2bb1aada9c247f29b968a019
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-09 16:15:08 +0200

    New: Enhance through adding animations to `AuthLayout` component

--- src/app/(app)/(auth)/layout.tsx
+++ src/app/(app)/(auth)/layout.tsx
@@ -1,28 +1,38 @@
-// REVIEWED - 03
+// REVIEWED - 04
 
 import Image from "next/image";
+import { redirect } from "next/navigation";
 import { PropsWithChildren } from "react";
 
-import { Public } from "@/components/auth/public";
+import { getAuth } from "@/actions/auth";
 import { Container } from "@/components/globals/container";
+import { MotionDiv } from "@/components/globals/motion";
+import { motions } from "@/lib/motion";
 
-export default function AuthLayout({ children }: PropsWithChildren) {
+export default async function AuthLayout({ children }: PropsWithChildren) {
+  const auth = await getAuth();
+
+  if (auth) if (auth.user) redirect("/");
+
   return (
-    <Public>
-      <div className="my-48 xs:my-40">
-        <Container className="max-w-lg">
+    <div className="flex h-full max-h-screen min-h-[48rem] items-center justify-center">
+      <Container>
+        <MotionDiv
+          initial={motions.fadeIn.initial}
+          animate={motions.fadeIn.whileInView}
+          transition={motions.transition({ duration: "fast" })}>
           <Image
             src="/pc-logo-primary-foreground.png"
             alt="PalestinianCauses Logo"
             sizes="3rem"
             fill
-            className="!static mx-auto mb-8 !h-auto !w-24 object-cover"
+            className="!static mx-auto mb-6 !h-auto !w-24 object-cover"
           />
-          <div className="flex flex-col items-stretch justify-center">
-            {children}
-          </div>
-        </Container>
-      </div>
-    </Public>
+        </MotionDiv>
+        <main className="mx-auto flex h-full max-w-lg flex-col items-stretch justify-center">
+          {children}
+        </main>
+      </Container>
+    </div>
   );
 }
