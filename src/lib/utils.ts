e5a83ee7d010737157265da0299b9578327aec20
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-09 16:49:40 +0200

    New: Enhance through adding `toHEX` function to `lib/utils.ts` file

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,15 @@
-// REVIEWED - 03
+// REVIEWED - 04
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 import { httpStatusesMessages, messages } from "./errors";
+
+export const toHEX = function toHEX(color: string) {
+  const rgb = color.split(" ").map(Number);
+  const hex = rgb.map((value) => value.toString(16).padStart(2, "0")).join("");
+
+  return ["#", hex].join("");
+};
 
 export const cn = function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
