44960cc81b4b9a73da33be79f7fac46dd9d16a98
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-11 00:02:18 +0200

    New: Enhance through adding `BackButton` component

---
+++ src/components/globals/back-button.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+// REVIEWED
+
+import { ArrowLeftIcon } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
+import { motions } from "@/lib/motion";
+
+import { Button } from "../ui/button";
+
+import { MotionDiv } from "./motion";
+
+export const BackButton = function BackButton() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  if (pathname === "/") return null;
+
+  return (
+    <MotionDiv
+      initial={motions.fadeIn.initial}
+      animate={motions.fadeIn.whileInView}
+      transition={motions.transition({})}>
+      <Button
+        variant="outline"
+        size="lg"
+        className="fixed left-5 top-5 z-50"
+        onClick={router.back}>
+        <ArrowLeftIcon />
+        <span>Go Back</span>
+      </Button>
+    </MotionDiv>
+  );
+};
