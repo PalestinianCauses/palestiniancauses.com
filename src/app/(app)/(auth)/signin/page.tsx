49c5fd4024a598716ef06eff31e8f2da83bb2826
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-11 13:48:49 +0200

    Update: Enhance through adding animations to `SignInPage`

--- src/app/(app)/(auth)/signin/page.tsx
+++ src/app/(app)/(auth)/signin/page.tsx
@@ -1,26 +1,36 @@
-// REVIEWED - 01
+// REVIEWED - 02
 
 import { Metadata } from "next";
 import { Fragment } from "react";
 
 import { SignInForm } from "@/components/auth/forms/sign-in";
+import { MotionH2, MotionP } from "@/components/globals/motion";
+import { motions } from "@/lib/motion";
 
 export const metadata: Metadata = {
-  title: "Sign In",
+  title: "Sign in",
 };
 
 export default function SignInPage() {
   return (
     <Fragment>
-      <div className="mb-10 flex flex-col items-center justify-center gap-3">
-        <h2 className="text-center text-2xl font-semibold leading-none">
+      <div className="mb-8 flex flex-col items-center justify-center gap-2.5">
+        <MotionH2
+          initial={motions.fadeIn.initial}
+          animate={motions.fadeIn.whileInView}
+          transition={motions.transition({ duration: "fast", delay: 0.1 })}
+          className="text-center text-2xl font-semibold leading-none tracking-tight">
           Welcome Back.
-        </h2>
-        <p className="text-center text-sm leading-normal text-muted-foreground">
+        </MotionH2>
+        <MotionP
+          initial={motions.fadeIn.initial}
+          animate={motions.fadeIn.whileInView}
+          transition={motions.transition({ duration: "fast", delay: 0.2 })}
+          className="text-center text-sm leading-normal text-muted-foreground">
           Let us be united together to share{" "}
-          <span className="font-medium text-foreground">Gaza&apos;s</span>{" "}
+          <span className="font-medium text-foreground">Gazans&apos;</span>{" "}
           voices.
-        </p>
+        </MotionP>
       </div>
       <SignInForm />
     </Fragment>
