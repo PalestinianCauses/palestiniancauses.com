6040cc1cf3504c8d75fe4cc58f9ebc0d0d5f6919
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-11 16:50:37 +0200

    New: Enhance through adding `HeaderButtons` component

---
+++ src/components/a-human-but-from-gaza/header-buttons.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+// REVIEWED
+
+import { ArrowRightIcon, HeartIcon } from "lucide-react";
+import Link from "next/link";
+import { useTransition } from "react";
+
+import {
+  createCartPlusSetCookie,
+  getCheckoutUrl,
+  insertItem,
+  removeCartPlusRemoveCookie,
+} from "@/actions/cart";
+import { motions } from "@/lib/motion";
+import { Product } from "@/lib/shopify/types";
+
+import { MotionDiv } from "../globals/motion";
+import { navigation } from "../globals/navigation";
+import { Button } from "../ui/button";
+
+export const HeaderButtons = function HeaderButtons({
+  product,
+}: {
+  product: Product;
+}) {
+  const [isPending, startTransition] = useTransition();
+
+  return (
+    <div className="flex w-full flex-col items-stretch justify-center gap-2.5 sm:w-max sm:flex-row sm:items-center sm:gap-5">
+      <MotionDiv
+        initial={motions.fadeIn.initial}
+        whileInView={motions.fadeIn.whileInView}
+        transition={motions.transition({ delay: 0.7 })}
+        className="w-full">
+        <Button
+          size="lg"
+          disabled={isPending}
+          className="w-full"
+          onClick={() => {
+            startTransition(async () => {
+              await createCartPlusSetCookie("internal-cart-id");
+              await insertItem("internal-cart-id", product.variants[0].id);
+
+              const checkoutUrl = await getCheckoutUrl("internal-cart-id");
+              window.open(checkoutUrl, "_blank");
+
+              await removeCartPlusRemoveCookie("internal-cart-id");
+            });
+          }}>
+          {isPending ? "Redirecting..." : "Order For Free"}
+          <ArrowRightIcon />
+        </Button>
+      </MotionDiv>
+      <MotionDiv
+        initial={motions.fadeIn.initial}
+        whileInView={motions.fadeIn.whileInView}
+        transition={motions.transition({ delay: 0.8 })}
+        className="w-full">
+        <Button variant="outline" size="lg" className="w-full" asChild>
+          <Link href={navigation[3].href} target="_blank">
+            Support Gazans
+            <HeartIcon />
+          </Link>
+        </Button>
+      </MotionDiv>
+    </div>
+  );
+};
