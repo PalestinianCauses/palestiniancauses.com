f6c020314b2e45a8f8c6233586c8da61d876e5e4
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-14 15:05:20 +0200

    New: Enhance through updating `BookPage` component to redirect to `AHumanButFromGazaPage`

--- src/app/(app)/book/page.tsx
+++ src/app/(app)/book/page.tsx
@@ -1,20 +1,7 @@
-// REVIEWED - 03
-import { Fragment } from "react";
+// REVIEWED - 04
 
-import { About } from "@/components/book/about";
-import { EarlyReviews } from "@/components/book/early-reviews";
-import { Hero } from "@/components/book/hero";
-import { Pricing } from "@/components/book/pricing";
-import { SneakPeak } from "@/components/book/sneak-peak";
+import { redirect } from "next/navigation";
 
 export default async function BookPage() {
-  return (
-    <Fragment>
-      <Hero />
-      <About />
-      <SneakPeak />
-      <EarlyReviews />
-      <Pricing />
-    </Fragment>
-  );
+  redirect("/a-human-but-from-gaza");
 }
