d9a905005f5cfd151c63f4502cb0ca3469d79c1b
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-18 13:53:52 +0200

    Update: Enhance through updating SEO description in `BookPage` component

--- src/app/(app)/book/page.tsx
+++ src/app/(app)/book/page.tsx
@@ -1,6 +1,13 @@
-// REVIEWED - 04
+// REVIEWED - 05
 
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "A Human But From Gaza",
+  description:
+    'Explore "A Human But From Gaza," an essential collection merging intimate diaries and powerful artwork. Witness firsthand the pain, resilience, and enduring hope of Gazans living through the war in Gaza. Amplify their authentic voices and stand in solidarity—order your copy today to support our mission.',
+};
 
 export default async function BookPage() {
   redirect("/a-human-but-from-gaza");
