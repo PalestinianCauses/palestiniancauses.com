63a402606744c9f8f177a3071871dad3faf78180
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-18 14:07:30 +0200

    Update: Enhance through updating `ReturnsPolicyPage` component UI

--- src/app/(app)/returns-policy/page.tsx
+++ src/app/(app)/returns-policy/page.tsx
@@ -1,86 +1,86 @@
-// REVIEWED
+// REVIEWED - 01
+import { Metadata } from "next";
 import Link from "next/link";
 
-import { HeroBackgroundPattern } from "@/components/book/hero-background-pattern";
 import { Container } from "@/components/globals/container";
+import {
+  Paragraph,
+  SectionHeading,
+  SectionHeadingBadge,
+  SubSectionHeading,
+} from "@/components/globals/typography";
 import { Button } from "@/components/ui/button";
+
+export const metadata: Metadata = {
+  title: "Returns Policy",
+  description: "PalestinianCauses LLC: Returns Policy",
+};
 
 export default function ReturnsPolicyPage() {
   return (
-    <main className="relative py-48">
-      <HeroBackgroundPattern />
-      <Container>
-        <section
-          id="returns-policy"
-          data-section="returns-policy"
-          className="mx-auto flex max-w-3xl flex-col gap-10">
-          <div className="flex flex-col items-start gap-2">
-            <h1
-              className="font-stretch text-balance bg-gradient-to-b from-foreground/25 via-foreground to-foreground/50 bg-cover bg-clip-text bg-center bg-no-repeat text-center text-4xl font-semibold tracking-tight text-foreground sm:text-5xl"
-              style={{ WebkitTextFillColor: "transparent" }}>
-              PalestinianCauses LLC Returns Policy.
-            </h1>
-          </div>
-          <div className="flex flex-col gap-10 text-lg/9 text-muted-foreground">
-            <p>Last updated: January 9, 2025</p>
-            <p>
-              We understand there may be a time to return a purchase, and we aim
-              to make the return process as simple as possible.
-            </p>
-            <p>
-              If you want to return or exchange your order, we offer returns
-              within 30 days of purchase. You can return your product for store
-              credit or a refund using the original payment method.
-            </p>
-            <p>
-              Please note: All returned items must be in new and unused
-              condition, with the original tags and labels attached.
-            </p>
-            <h2 className="mt-6 text-3xl font-semibold text-foreground">
-              Return Process.
-            </h2>
-            <p>
-              To return an item, email us at{" "}
-              <span>
-                <Button
-                  variant="link"
-                  className="p-0 text-lg/9 font-medium text-foreground"
-                  asChild>
-                  <Link href="mailto:hello@palestiniancauses.com">
-                    hello@palestiniancauses.com
-                  </Link>
-                </Button>
-              </span>{" "}
-              with your Order Number and return reason, and our support team
-              will be happy to assist you with the next steps.
-            </p>
-            <h2 className="mt-6 text-3xl font-semibold text-foreground">
-              Refunds.
-            </h2>
-            <p>
-              After receiving your item/s and inspecting the condition, we will
-              process your refund or exchange. Please allow at least 3 days from
-              receiving your item/s to process your return.
-            </p>
-            <h2 className="mt-6 text-3xl font-semibold text-foreground">
-              Questions.
-            </h2>
-            <p>
-              For questions about our returns policy, please contact us at{" "}
-              <span>
-                <Button
-                  variant="link"
-                  className="p-0 text-lg/9 font-medium text-foreground"
-                  asChild>
-                  <Link href="mailto:hello@palestiniancauses.com">
-                    hello@palestiniancauses.com
-                  </Link>
-                </Button>
-              </span>{" "}
-              .
-            </p>
-          </div>
-        </section>
+    <main className="relative py-32 xl:py-48">
+      <Container className="max-w-4xl">
+        <SectionHeading className="mb-6">Returns Policy.</SectionHeading>
+        <SectionHeadingBadge className="mb-4">
+          This policy is effective as of January 9, 2025
+        </SectionHeadingBadge>
+        <SectionHeadingBadge className="mb-12">
+          Last updated: January 9, 2025
+        </SectionHeadingBadge>
+        <div className="flex flex-col items-start justify-start gap-8">
+          <Paragraph>
+            We understand there may be a time to return a purchase, and we aim
+            to make the return process as simple as possible.
+          </Paragraph>
+          <Paragraph>
+            If you want to return or exchange your order, we offer returns
+            within 30 days of purchase. You can return your product for store
+            credit or a refund using the original payment method.
+          </Paragraph>
+          <Paragraph>
+            Please note: All returned items must be in new and unused condition,
+            with the original tags and labels attached.
+          </Paragraph>
+          <SubSectionHeading>Return Process.</SubSectionHeading>
+          <Paragraph>
+            To return an item, email us at{" "}
+            <span>
+              <Button
+                variant="link"
+                className="p-0 text-lg/9 font-medium text-foreground"
+                style={{ fontSize: "inherit" }}
+                asChild>
+                <Link href="mailto:hello@palestiniancauses.com">
+                  hello@palestiniancauses.com
+                </Link>
+              </Button>
+            </span>{" "}
+            with your Order Number and return reason, and our support team will
+            be happy to assist you with the next steps.
+          </Paragraph>
+          <SubSectionHeading>Refunds.</SubSectionHeading>
+          <Paragraph>
+            After receiving your item/s and inspecting the condition, we will
+            process your refund or exchange. Please allow at least 3 days from
+            receiving your item/s to process your return.
+          </Paragraph>
+          <SubSectionHeading>Questions.</SubSectionHeading>
+          <Paragraph>
+            For questions about our returns policy, please contact us at{" "}
+            <span>
+              <Button
+                variant="link"
+                className="p-0 text-lg/9 font-medium text-foreground"
+                style={{ fontSize: "inherit" }}
+                asChild>
+                <Link href="mailto:hello@palestiniancauses.com">
+                  hello@palestiniancauses.com
+                </Link>
+              </Button>
+            </span>{" "}
+            .
+          </Paragraph>
+        </div>
       </Container>
     </main>
   );
