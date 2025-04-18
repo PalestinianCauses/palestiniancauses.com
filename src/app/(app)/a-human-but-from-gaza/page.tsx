f871f09c84830668efb3e95a66f29c4ce78ce9fb
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-18 13:52:44 +0200

    Update: Enhance through updating SEO description in `AHumanButFromGazaPage` component

--- src/app/(app)/a-human-but-from-gaza/page.tsx
+++ src/app/(app)/a-human-but-from-gaza/page.tsx
@@ -1,11 +1,19 @@
-// REVIEWED - 01
+// REVIEWED - 02
 
+import { Metadata } from "next";
 import { Fragment } from "react";
 
 import { About } from "@/components/a-human-but-from-gaza/about";
+import { Footer } from "@/components/a-human-but-from-gaza/footer";
 import { Header } from "@/components/a-human-but-from-gaza/header";
 import { Testimonials } from "@/components/a-human-but-from-gaza/testimonials";
 import { getProduct } from "@/lib/shopify";
+
+export const metadata: Metadata = {
+  title: "A Human But From Gaza",
+  description:
+    'Explore "A Human But From Gaza," an essential collection merging intimate diaries and powerful artwork. Witness firsthand the pain, resilience, and enduring hope of Gazans living through the war in Gaza. Amplify their authentic voices and stand in solidarity—order your copy today to support our mission.',
+};
 
 export default async function AHumanButFromGazaPage() {
   const product = await getProduct("a-human-but-from-gaza-e-book");
@@ -16,7 +24,7 @@ export default async function AHumanButFromGazaPage() {
       <Header product={product} />
       <About />
       <Testimonials />
-      <div className="py-20" />
+      <Footer product={product} />
     </Fragment>
   );
 }
