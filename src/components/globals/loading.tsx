e59384601584aa14aa8aa3c599b6dd83799a2f36
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-25 11:47:38 +0200

    Update: Enhance through adding `className` prop to `globals/loading.tsx`

--- src/components/globals/loading.tsx
+++ src/components/globals/loading.tsx
@@ -1,10 +1,19 @@
-// REVIEWED - 01
+// REVIEWED - 02
 
 import { Loader2 } from "lucide-react";
+import { HTMLAttributes } from "react";
 
-export const Loading = function Loading() {
+import { cn } from "@/lib/utils";
+
+export const Loading = function Loading({
+  className,
+}: HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className="flex h-full max-h-screen min-h-[40rem] w-full items-center justify-center">
+    <div
+      className={cn(
+        "flex h-full max-h-screen min-h-[40rem] w-full items-center justify-center",
+        className,
+      )}>
       <Loader2 className="h-10 w-10 animate-spin stroke-1 text-primary" />
     </div>
   );
