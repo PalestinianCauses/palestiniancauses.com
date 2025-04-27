0b2896e2f086c6075bbd4009a0d30d5e09811364
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-27 23:40:31 +0200

    New: Enhance through adding a new but empty migration

---
+++ src/migrations/20250427_213203.ts
@@ -0,0 +1,8 @@
+// REVIEWED
+export const up = async function up(): Promise<void> {
+  // Migration code
+};
+
+export const down = async function down(): Promise<void> {
+  // Migration code
+};
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,10 @@
-// REVIEWED
+// REVIEWED - 01
 
 /* eslint-disable camelcase */
 
 import * as migration_20250401_231101_02_04_2025 from "./20250401_231101_02_04_2025";
 import * as migration_20250420_225253_remove_frappe_columns_and_update_status_enum from "./20250420_225253_remove_frappe_columns_and_update_status_enum";
+import * as migration_20250427_213203 from "./20250427_213203";
 
 export const migrations = [
   {
@@ -15,5 +16,10 @@ export const migrations = [
     up: migration_20250420_225253_remove_frappe_columns_and_update_status_enum.up,
     down: migration_20250420_225253_remove_frappe_columns_and_update_status_enum.down,
     name: "20250420_225253_remove_frappe_columns_and_update_status_enum",
+  },
+  {
+    up: migration_20250427_213203.up,
+    down: migration_20250427_213203.down,
+    name: "20250427_213203",
   },
 ];
