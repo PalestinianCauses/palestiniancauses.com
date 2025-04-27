e2e2031d4d331b0ba0071063ad03ca2b97dd734b
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-28 00:39:49 +0200

    Update: Enhance through updating date validation in Diary Entry collection

--- src/collections/DiaryEntries.ts
+++ src/collections/DiaryEntries.ts
@@ -1,9 +1,11 @@
-// REVIEWED - 05
+// REVIEWED - 06
 
+import { revalidatePath } from "next/cache";
 import { CollectionConfig } from "payload";
 
 import { isAdminOrSystemUserOrSelf } from "@/access/diary-entry";
 import { isAdmin, isAdminOrSystemUserField } from "@/access/global";
+import { messages } from "@/lib/errors";
 
 export const DiaryEntries: CollectionConfig = {
   slug: "diary-entries",
@@ -19,6 +21,7 @@ export const DiaryEntries: CollectionConfig = {
       label: "Title",
       name: "title",
       type: "text",
+      minLength: 2,
       maxLength: 100,
       required: true,
       unique: true,
@@ -28,6 +31,32 @@ export const DiaryEntries: CollectionConfig = {
       name: "date",
       type: "date",
       required: true,
+      validate: (value) => {
+        if (!value) return messages.forms.required("diary date");
+
+        const date = new Date(value);
+        date.setUTCHours(0, 0, 0, 0);
+
+        if (!(date instanceof Date) || Number.isNaN(date.getTime()))
+          return messages.forms.valid("diary date");
+
+        const today = new Date();
+        today.setUTCHours(0, 0, 0, 0);
+
+        const yesterday = new Date(today);
+        yesterday.setUTCDate(today.getDate() - 1);
+
+        const october7th2023 = new Date(2023, 9, 7);
+        october7th2023.setUTCHours(0, 0, 0, 0);
+
+        if (
+          date.getTime() > yesterday.getTime() ||
+          date.getTime() <= october7th2023.getTime()
+        )
+          return messages.forms.diaryEntry.date("Oct 7th. 2023", "yesterday");
+
+        return true;
+      },
     },
     {
       label: "Content",
@@ -77,4 +106,24 @@ export const DiaryEntries: CollectionConfig = {
       required: true,
     },
   ],
+  hooks: {
+    beforeChange: [
+      async ({ req, data }) => {
+        const document = data;
+        if (
+          req.user &&
+          (req.user.role === "admin" || req.user.role === "system-user")
+        )
+          document.status = "approved";
+
+        return document;
+      },
+    ],
+    afterChange: [
+      async () => {
+        revalidatePath("/humans-but-from-gaza");
+        console.log("Route Revalidated.");
+      },
+    ],
+  },
 };
