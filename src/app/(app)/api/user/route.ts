f92690abb10294fdeb815264970d01f0eb0bc7f7
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-28 00:43:23 +0200

    Update: Enhance through refactoring API user route with `actionSafeExecute` function

--- src/app/(app)/api/user/route.ts
+++ src/app/(app)/api/user/route.ts
@@ -1,22 +1,16 @@
-// REVIEWED - 01
+// REVIEWED - 02
 
 import { deleteUser } from "@/actions/user";
 import { messages } from "@/lib/errors";
 
 export const DELETE = async function DELETE(request: Request) {
-  const response = {
-    data: null as null | string,
-    error: null as null | string,
-  };
-
   const { headers, json } = request;
 
   if (
     headers.get("X-Testing-Clean-Up-Secret") !==
     process.env.PLAYWRIGHT_TESTING_USER_CLEAN_UP_SECRET
   ) {
-    response.error = messages.http.unAuthorized;
-    return new Response(JSON.stringify(response.error), {
+    return new Response(JSON.stringify(messages.http.unAuthorized), {
       status: 401,
     });
   }
@@ -24,27 +18,13 @@ export const DELETE = async function DELETE(request: Request) {
   const body = (await json()) as { email: string };
   const deleteResponse = await deleteUser(body.email);
 
-  if (deleteResponse.error) {
-    response.error = deleteResponse.error;
-    return new Response(JSON.stringify(response.error), {
+  if (!deleteResponse.data || deleteResponse.error) {
+    return new Response(JSON.stringify(deleteResponse.error), {
       status: 400,
     });
   }
 
-  response.data = deleteResponse.data;
-
-  if (
-    (response.data && response.error) ||
-    (!response.data && !response.error)
-  ) {
-    response.data = null;
-    response.error = messages.http.serverError;
-    return new Response(JSON.stringify(response.error), {
-      status: 500,
-    });
-  }
-
-  return new Response(JSON.stringify(response.data), {
+  return new Response(JSON.stringify(deleteResponse.data), {
     status: 200,
   });
 };
