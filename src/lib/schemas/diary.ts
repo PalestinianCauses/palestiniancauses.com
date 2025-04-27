bc32cad54d2d8f4fb13013c9343267a350dbf1e6
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-28 00:41:36 +0200

    Update: Enhance through updating date validation in Diary Entry schema

--- src/lib/schemas/diary.ts
+++ src/lib/schemas/diary.ts
@@ -1,19 +1,51 @@
-// REVIEWED - 01
+// REVIEWED - 02
 
 import { z } from "zod";
 
 import { messages } from "../errors";
 
 export const diaryEntrySchema = z.object({
-  title: z.string().min(2, messages.forms.required("diary title")),
+  title: z
+    .string()
+    .min(2, messages.forms.required("diary title"))
+    .max(100, messages.forms.maxLength("diary title", 100)),
   date: z
-    .date({ message: messages.forms.required("diary date") })
-    .min(new Date("2023-07-10"), messages.forms.date("Oct 07, 2023", "today"))
-    .max(
-      new Date(Date.now() - 24 * 60 * 60 * 1000),
-      messages.forms.date("Oct 10, 2023", "yesterday"),
+    .preprocess(
+      (value) => {
+        if (value instanceof Date) return value;
+        if (typeof value === "string") {
+          const date = new Date(value);
+          return Number.isNaN(date.getTime()) ? value : date;
+        }
+
+        return value;
+      },
+      z.date({ invalid_type_error: messages.forms.valid("diary date") }),
+    )
+    .refine(
+      (date) => {
+        const today = new Date();
+        today.setUTCHours(0, 0, 0, 0);
+
+        const yesterday = new Date(today);
+        yesterday.setUTCDate(today.getUTCDate() - 1);
+
+        const october7th2023 = new Date(2023, 9, 7);
+        october7th2023.setUTCHours(0, 0, 0, 0);
+
+        return (
+          date.getTime() <= yesterday.getTime() &&
+          date.getTime() > october7th2023.getTime()
+        );
+      },
+      {
+        message: messages.forms.diaryEntry.date(
+          "October 7th. 2023",
+          "yesterday",
+        ),
+      },
     ),
-  content: z.string().min(2500, messages.forms.content),
+  content: z.string().min(2500, messages.forms.diaryEntry.content),
   isAuthentic: z.boolean(),
   isAnonymous: z.boolean(),
 });
