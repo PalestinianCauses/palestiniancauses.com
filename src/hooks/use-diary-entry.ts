f5675390466ac6996e914674b92ee8a13e17bfee
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-28 00:56:35 +0200

    Update: Enhance through refactoring diary entry hook with after actions refactoring

--- src/hooks/use-diary-entry.ts
+++ src/hooks/use-diary-entry.ts
@@ -1,11 +1,16 @@
-// REVIEWED
+// REVIEWED - 01
 
 import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 import { createDiaryEntry } from "@/actions/diary-entry";
+import { queryClient } from "@/app/(app)/providers";
 import { DiaryEntry } from "@/payload-types";
 
 export const useDiaryEntry = function useDiaryEntry() {
+  const router = useRouter();
+
   const createDiaryEntryMutation = useMutation({
     mutationFn: async (
       diaryEntryData: Omit<
@@ -16,7 +21,17 @@ export const useDiaryEntry = function useDiaryEntry() {
       const response = await createDiaryEntry(diaryEntryData);
       return response;
     },
+    onSuccess: (response) => {
+      if (!response.data || response.error) {
+        toast.error(response.error);
+        return;
+      }
+
+      toast.success(response.data);
+      queryClient.invalidateQueries({ queryKey: ["diary-entry"] });
+      router.push("/humans-but-from-gaza");
+    },
   });
 
-  return { createDiaryEntryMutation };
+  return { createDiaryEntry: createDiaryEntryMutation };
 };
