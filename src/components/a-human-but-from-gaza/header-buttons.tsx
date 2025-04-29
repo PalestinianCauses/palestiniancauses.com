b0512fe0a8b5be554acc041a6e9e3dc11c6017bd
Shawqi Hatem <shawqicauses@outlook.com>
2025-04-29 04:36:52 +0200

    Update: Enhance through using products action in header buttons component

--- src/components/a-human-but-from-gaza/header-buttons.tsx
+++ src/components/a-human-but-from-gaza/header-buttons.tsx
@@ -1,30 +1,26 @@
 "use client";
 
-// REVIEWED - 03
+// REVIEWED - 04
 
-import { ArrowRightIcon, HeartIcon } from "lucide-react";
+import { ArrowRightIcon, DownloadIcon, HeartIcon } from "lucide-react";
 import Link from "next/link";
-import { useTransition } from "react";
+import { useState, useTransition } from "react";
+import { toast } from "sonner";
 
-import {
-  createCartPlusSetCookie,
-  getCheckoutUrl,
-  insertItem,
-  removeCartPlusRemoveCookie,
-} from "@/actions/cart";
+import { getFreeProductFiles } from "@/actions/product";
 import { motions } from "@/lib/motion";
-import { Product } from "@/lib/shopify/types";
+import { Media } from "@/payload-types";
 
 import { MotionDiv } from "../globals/motion";
 import { navigation } from "../globals/navigation";
 import { Button } from "../ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
-export const HeaderButtons = function HeaderButtons({
-  product,
-}: {
-  product: Product;
-}) {
+export const HeaderButtons = function HeaderButtons() {
   const [isPending, startTransition] = useTransition();
+  const [files, setFiles] = useState<
+    { title: string; file: Omit<Media, "url"> & { url: string } }[]
+  >([]);
 
   return (
     <div className="flex w-full flex-col items-stretch justify-center gap-2.5 sm:w-max sm:flex-row sm:items-center sm:gap-5">
@@ -34,24 +30,69 @@ export const HeaderButtons = function HeaderButtons({
         animate={motions.fadeIn.whileInView}
         transition={motions.transition({ delay: 0.4 })}
         className="w-full">
-        <Button
-          size="lg"
-          disabled={isPending}
-          className="w-full"
-          onClick={() => {
-            startTransition(async () => {
-              await createCartPlusSetCookie("internal-cart-id");
-              await insertItem("internal-cart-id", product.variants[0].id);
+        <Popover defaultOpen={files.length > 0}>
+          <PopoverTrigger asChild>
+            <Button
+              size="lg"
+              disabled={isPending}
+              className="w-full"
+              onClick={() => {
+                if (files.length) return;
 
-              const checkoutUrl = await getCheckoutUrl("internal-cart-id");
-              window.location.href = checkoutUrl;
+                startTransition(async () => {
+                  const responseProductFiles = await getFreeProductFiles(
+                    "a-human-but-from-gaza",
+                    "/a-human-but-from-gaza",
+                  );
 
-              await removeCartPlusRemoveCookie("internal-cart-id");
-            });
-          }}>
-          {isPending ? "Redirecting..." : "Order For Free"}
-          <ArrowRightIcon />
-        </Button>
+                  if (
+                    !responseProductFiles.data ||
+                    responseProductFiles.error
+                  ) {
+                    toast.error(responseProductFiles.error);
+                    return;
+                  }
+
+                  setFiles(responseProductFiles.data);
+                });
+              }}>
+              {isPending ? "Ordering..." : "Order For Free"}
+              <ArrowRightIcon />
+            </Button>
+          </PopoverTrigger>
+          {files.length > 0 && (
+            <PopoverContent
+              side="top"
+              align="start"
+              sideOffset={12}
+              className="w-72 max-w-lg p-0 sm:w-[32rem]">
+              <ul>
+                {files.filter(Boolean).map((file) => (
+                  <li key={file.file.id}>
+                    <Button
+                      variant="ghost"
+                      size="lg"
+                      className="w-full justify-between"
+                      asChild>
+                      <Link href={file.file.url}>
+                        <span className="truncate">
+                          {file.title}
+                          {file.file.filesize ? (
+                            <span className="ml-2 font-mono text-sm text-muted-foreground">
+                              ({Math.round(file.file.filesize / 1024 / 1024)}{" "}
+                              MB)
+                            </span>
+                          ) : null}
+                        </span>
+                        <DownloadIcon className="!h-5 !w-5 stroke-[1.5]" />
+                      </Link>
+                    </Button>
+                  </li>
+                ))}
+              </ul>
+            </PopoverContent>
+          )}
+        </Popover>
       </MotionDiv>
       <MotionDiv
         viewport={{ once: true }}
@@ -60,7 +101,7 @@ export const HeaderButtons = function HeaderButtons({
         transition={motions.transition({ delay: 0.5 })}
         className="w-full">
         <Button variant="outline" size="lg" className="w-full" asChild>
-          <Link href={navigation[3].href} target="_blank">
+          <Link href={navigation[3].href}>
             Support Gazans
             <HeartIcon />
           </Link>
