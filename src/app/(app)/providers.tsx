18db25f2cb57785e07f700031471a7cc3e3d77f5
Shawqi Hatem <shawqicauses@outlook.com>
2025-05-06 15:57:15 +0200

    New: Enhance through adding `ServiceWorkerRegistrationLogger` component

--- src/app/(app)/providers.tsx
+++ src/app/(app)/providers.tsx
@@ -1,9 +1,22 @@
 "use client";
 
-// REVIEWED - 03
+// REVIEWED - 04
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect } from "react";
+
+export const ServiceWorkerRegistrationLogger =
+  function ServiceWorkerRegistrationLogger() {
+    useEffect(() => {
+      if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.ready
+          .then((register) => console.log("[Service Worker]: Ready:", register))
+          .catch((error) => console.error("[Service Worker]: Error:", error));
+      }
+    }, []);
+
+    return null;
+  };
 
 export const queryClient = new QueryClient({
   defaultOptions: {
