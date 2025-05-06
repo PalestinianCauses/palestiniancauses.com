892c607eff1d127b0d068096b1f1085622c80ec5
Shawqi Hatem <shawqicauses@outlook.com>
2025-05-06 15:53:06 +0200

    New: Enhance through adding manifest meta data tags

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,7 @@
-// REVIEWED - 20
-import type { Metadata } from "next";
+// REVIEWED - 21
+import type { Metadata, Viewport } from "next";
 import { PropsWithChildren } from "react";
+import colors from "tailwindcss/colors";
 
 import { BackButton } from "@/components/globals/back-button";
 import { Toaster } from "@/components/ui/sonner";
@@ -8,6 +9,7 @@ import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  applicationName: "PalestinianCauses",
   title: { template: "%s | PalestinianCauses", default: "PalestinianCauses" },
   description:
     'PalestinianCauses LLC is a mission-driven creative and digital platform dedicated to illuminating the Gazan experience, with an urgent focus on amplifying authentic voices and realities from Gaza during the current crisis. Through compelling storytelling, evocative artwork (as we showcased in our project "A Human But From Gaza"), and innovative digital solutions developed by our dedicated team, we strive to build global solidarity and foster deep empathy.',
@@ -40,7 +42,41 @@ export const metadata: Metadata = {
 
   robots: { index: true, follow: true },
 
-  icons: "/logo-primary.png",
+  icons: [
+    {
+      rel: "shortcut icon",
+      type: "image/x-icon",
+      url: "/favicon.ico",
+    },
+    {
+      rel: "icon",
+      type: "image/svg+xml",
+      url: "/icon-01.svg",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "96x96",
+      url: "/icon-01.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      type: "image/png",
+      sizes: "180x180",
+      url: "/apple.png",
+    },
+  ],
+
+  manifest: "/manifest.json",
+
+  appleWebApp: {
+    title: "PC",
+    capable: true,
+    statusBarStyle: "black-translucent",
+    startupImage: "/logo-primary.png",
+  },
+
+  formatDetection: { telephone: true },
 
   openGraph: {
     siteName: "PalestinianCauses",
@@ -64,9 +100,30 @@ export const metadata: Metadata = {
   },
 };
 
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  themeColor: colors.zinc["900"],
+};
+
 const RootLayout = async function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <meta name="mobile-web-app-capable" content="yes" />
+        <link rel="apple-touch-icon" href="/apple.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="192x192"
+          href="/manifest-192x192.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="512x512"
+          href="/manifest-512x512.png"
+        />
+      </head>
       <body>
         <BackButton />
         {children}
