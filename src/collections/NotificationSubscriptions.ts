ae888976612b29bac82e2b9ab17380a01f4246da
Shawqi Hatem <shawqicauses@outlook.com>
2025-05-08 14:32:08 +0200

    New: Enhance through adding `NotificationSubscriptions` collection

---
+++ src/collections/NotificationSubscriptions.ts
@@ -0,0 +1,51 @@
+// REVIEWED
+
+import { CollectionConfig } from "payload";
+
+import { isAdmin } from "@/access/global";
+
+export const NotificationSubscriptions: CollectionConfig = {
+  slug: "notification-subscriptions",
+  access: {
+    read: isAdmin,
+    create: isAdmin,
+    update: isAdmin,
+    delete: isAdmin,
+  },
+  admin: { useAsTitle: "endpoint" },
+  fields: [
+    {
+      label: "Endpoint",
+      name: "endpoint",
+      type: "text",
+      required: true,
+      unique: true,
+    },
+    {
+      label: "Keys",
+      name: "keys",
+      type: "group",
+      fields: [
+        {
+          label: "P256DH Key",
+          name: "p256dh",
+          type: "text",
+          required: true,
+        },
+        {
+          label: "Auth Key",
+          name: "auth",
+          type: "text",
+          required: true,
+        },
+      ],
+    },
+    {
+      admin: { readOnly: true },
+      label: "User Agent",
+      name: "userAgent",
+      type: "text",
+      required: true,
+    },
+  ],
+};
