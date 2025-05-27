0db71437173ce1b16754ca94a16d45733014942c
Shawqi Hatem <shawqicauses@outlook.com>
2025-05-27 02:18:54 +0200

    Update: Enhance through refactoring user API delete end-point for testing purposes

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -1,21 +1,20 @@
 "use server";
 
-// REVIEWED - 05
+// REVIEWED - 06
 
 import { messages } from "@/lib/messages";
-import { actionSafeExecute } from "@/lib/network";
 import { payload } from "@/lib/payload";
 import { SignUpSchema } from "@/lib/schemas/auth";
-import { ResponseSafeExecute } from "@/lib/types";
 import { SafeExecuteError } from "@/lib/types/guards";
 import { isResilientPassword } from "@/lib/utils/passwords";
 
 export const getUserByEmail = async function getUserByEmail(email: string) {
+  const query = { email: { equals: email } };
   const response = await payload.find({
     collection: "users",
-    where: { email: { equals: email } },
+    where: query,
     overrideAccess: false,
-    req: { query: { email: { equals: email } } },
+    req: { query },
   });
 
   return response;
@@ -35,31 +34,4 @@ export const createUser = async function createUser(data: SignUpSchema) {
   });
 
   return response;
-};
-
-const deleteUserPayload = async function deleteUserPayload(email: string) {
-  const response = await payload.delete({
-    collection: "users",
-    where: { email: { equals: email } },
-  });
-
-  return response;
-};
-
-export const deleteUser = async function deleteUser(
-  email: string,
-): Promise<ResponseSafeExecute<string>> {
-  const responsePayload = await actionSafeExecute(
-    deleteUserPayload(email),
-    messages.actions.user.delete.serverError,
-  );
-
-  if (
-    !responsePayload.data ||
-    responsePayload.data.errors.length > 0 ||
-    responsePayload.error
-  )
-    return { data: null, error: messages.actions.user.delete.serverError };
-
-  return { data: messages.actions.user.delete.success, error: null };
 };
