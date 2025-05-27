05732cb06fe5efee8d4595071886411ee09dbd9f
Shawqi Hatem <shawqicauses@outlook.com>
2025-05-27 02:05:25 +0200

    Update: Enhance through updating Next.JS config file server actions allowed origins

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
-// REVIEWED - 12
+// REVIEWED - 13
 import { withPayload } from "@payloadcms/next/withPayload";
 import withSerwistInit from "@serwist/next";
 import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // server
+  experimental: {
+    serverActions: {
+      allowedOrigins: [process.env.NEXT_PUBLIC_DOMAIN!],
+    },
+  },
+
   eslint: { ignoreDuringBuilds: true },
+
   images: {
     remotePatterns: [
       {
