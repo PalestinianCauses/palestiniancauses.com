7e657c2eaf9c83ee66947657c501868ac70b502e
Shawqi Hatem <shawqicauses@outlook.com>
2025-06-11 04:26:42 +0200

    Update: Enhance through refactoring A Human But From Gaza header component to use Vercel Blobs

--- src/components/a-human-but-from-gaza/header.tsx
+++ src/components/a-human-but-from-gaza/header.tsx
@@ -1,18 +1,73 @@
-// REVIEWED - 06
+// REVIEWED - 07
 
 import Image from "next/image";
+import { Suspense } from "react";
 
+import { getBlobsByPrefix } from "@/actions/blob";
 import { motions } from "@/lib/motion";
 import { cn } from "@/lib/utils/styles";
 
-/* eslint-disable import/extensions */
-import pagesImagesBlurHashes from "../../../data/book-pages-images-blur-hashes.json";
 import { Container } from "../globals/container";
 import { InfiniteMarquee, MarqueeItem } from "../globals/marquee";
 import { MotionDiv, MotionP, MotionSpan } from "../globals/motion";
 import { Badge } from "../ui/badge";
+import { Skeleton } from "../ui/skeleton";
 
 import { HeaderButtons } from "./header-buttons";
+
+const HeaderImages = async function HeaderImages() {
+  const { data: images } = await getBlobsByPrefix("book-pages-images/");
+
+  if (!images || images.blobs.filter((blob) => blob.size !== 0).length === 0)
+    return null;
+
+  return (
+    <InfiniteMarquee speed={80}>
+      {images.blobs
+        .filter((blob) => blob.size !== 0)
+        .map(async (blob, index) => (
+          <MarqueeItem key={blob.pathname} delay={index * 0.1}>
+            <Image
+              src={blob.url}
+              alt={`Book Image ${index.toString()}`}
+              fill
+              sizes="(min-width: 64rem) 12rem, (min-width: 48rem) 10rem, 8rem"
+              placeholder="empty"
+              className="!relative aspect-[2/3] !w-60 max-w-none object-cover opacity-20 lg:!w-80"
+            />
+          </MarqueeItem>
+        ))}
+    </InfiniteMarquee>
+  );
+};
+
+const HeaderCover = async function HeaderCover() {
+  const { data: cover } = await getBlobsByPrefix("book-cover/");
+
+  if (!cover || cover.blobs.filter((blob) => blob.size !== 0).length === 0)
+    return null;
+
+  return (
+    <div className="absolute left-1/2 top-1/2 z-20 aspect-[2/3] h-auto w-72 -translate-x-1/2 -translate-y-1/2 lg:w-96">
+      <MotionDiv
+        viewport={{ once: true }}
+        initial={motions.fadeIn.initial}
+        animate={motions.fadeIn.whileInView}
+        transition={motions.transition({ delay: 0.3 })}
+        className="relative h-full w-full border border-muted">
+        <Image
+          src={cover.blobs.filter((blob) => blob.size !== 0)[0].url}
+          alt="Book Cover"
+          priority
+          fill
+          placeholder="empty"
+          sizes="(min-width: 64rem) 14rem, (min-width: 48rem) 12rem, 10rem"
+          className="!relative object-cover object-left"
+        />
+      </MotionDiv>
+    </div>
+  );
+};
 
 export const Header = function Header() {
   return (
@@ -80,39 +135,17 @@ export const Header = function Header() {
         transition={motions.transition({})}
         className="relative max-w-full border-y border-muted py-5">
         <div className="absolute left-1/2 top-1/2 z-20 aspect-[2/3] h-auto w-72 -translate-x-1/2 -translate-y-1/2 lg:w-96">
-          <MotionDiv
-            viewport={{ once: true }}
-            initial={motions.fadeIn.initial}
-            animate={motions.fadeIn.whileInView}
-            transition={motions.transition({ delay: 0.3 })}
-            className="relative h-full w-full border border-muted">
-            <Image
-              src={`https://qwvvvruhbe.ufs.sh/f/${pagesImagesBlurHashes[0].id}`}
-              alt="Book Cover"
-              priority
-              fill
-              placeholder="blur"
-              blurDataURL={pagesImagesBlurHashes[0].base64}
-              sizes="(min-width: 1024px) 16rem, (min-width: 768px) 14rem, 12rem"
-              className="!relative object-cover object-left"
-            />
-          </MotionDiv>
+          <Suspense
+            fallback={<Skeleton className="h-full w-full bg-primary/5" />}>
+            <HeaderCover />
+          </Suspense>
         </div>
-        <InfiniteMarquee speed={80}>
-          {pagesImagesBlurHashes.slice(1).map(async (image, index) => (
-            <MarqueeItem key={image.id} delay={index * 0.1}>
-              <Image
-                src={`https://qwvvvruhbe.ufs.sh/f/${image.id}`}
-                alt={`Book Image ${index.toString()}`}
-                fill
-                sizes="(min-width: 1024px) 12rem, (min-width: 768px) 10rem, 8rem"
-                placeholder="blur"
-                blurDataURL={image.base64}
-                className="!relative aspect-[2/3] !w-60 max-w-none object-cover opacity-20 lg:!w-80"
-              />
-            </MarqueeItem>
-          ))}
-        </InfiniteMarquee>
+        <Suspense
+          fallback={
+            <Skeleton className="h-[22.5rem] w-full bg-primary/5 lg:h-[30rem]" />
+          }>
+          <HeaderImages />
+        </Suspense>
       </MotionDiv>
     </header>
   );
