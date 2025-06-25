03a1859438b38c853315da4d86f57baa702fd7b0
Shawqi Hatem <shawqicauses@outlook.com>
2025-06-25 11:40:42 +0200

    Update: Enhance through removing border radius from accordion component

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-// REVIEWED
+// REVIEWED - 01
 
 import * as AccordionPrimitive from "@radix-ui/react-accordion";
 import { ChevronDown } from "lucide-react";
@@ -36,7 +36,7 @@ const AccordionTrigger = React.forwardRef<
       )}
       {...props}>
       {children}
-      <ChevronDown className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+      <ChevronDown className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-100" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ));
