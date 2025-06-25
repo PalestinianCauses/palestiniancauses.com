add1cd221330b0435bb3e86fc231f655c4118993
Shawqi Hatem <shawqicauses@outlook.com>
2025-06-25 21:36:48 +0200

    Update: Enhance through fixing transition timing function in globals/navigation-link component

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,4 +1,4 @@
-// REVIEWED - 02
+// REVIEWED - 03
 
 import * as React from "react";
 
@@ -11,7 +11,7 @@ const Textarea = React.forwardRef<
   <textarea
     ref={ref}
     className={cn(
-      "flex min-h-[3.75rem] w-full bg-background px-2.5 py-2.5 text-base leading-7 text-foreground ring-1 ring-input transition-all duration-100 ease-in placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm/6",
+      "flex min-h-[3.75rem] w-full bg-background px-2.5 py-2.5 text-base leading-7 text-foreground ring-1 ring-input transition-all duration-100 ease-in-out placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm/6",
       className,
     )}
     {...props}
