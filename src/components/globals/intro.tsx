0f60af04985ad83f5aa3327ac35afff244427489
Shawqi Hatem <shawqicauses@outlook.com>
2025-06-25 23:35:23 +0200

    Update: Enhance through improving globals/intro component

--- src/components/globals/intro.tsx
+++ src/components/globals/intro.tsx
@@ -1,18 +1,11 @@
-// REVIEWED - 09
+// REVIEWED - 10
 
 import Link from "next/link";
-
-import { getAuthentication } from "@/actions/auth";
-import { QueryProvider } from "@/app/(app)/providers";
-import { motions } from "@/lib/motion";
-import { cn } from "@/lib/utils/styles";
 
 import { AuthenticationButtons } from "../auth/buttons";
 import { NotificationButton } from "../notification/button";
 import { Button } from "../ui/button";
 import { Label } from "../ui/label";
-
-import { MotionDiv, MotionP, MotionSpan } from "./motion";
 
 const navigation = [
   { label: "Privacy Policy", href: "/privacy-policy" },
@@ -20,80 +13,33 @@ const navigation = [
 ];
 
 export const Intro = async function Intro() {
-  const authentication = await getAuthentication();
-
   return (
-    <section className="grid snap-start grid-rows-[max-content_1fr_max-content] gap-5 p-5 xl:gap-10 xl:p-10">
+    <section className="grid grid-rows-[max-content_1fr_max-content] gap-5 p-5 xl:gap-10 xl:p-10">
       <nav className="flex flex-wrap items-center justify-between gap-5">
-        <MotionDiv
-          viewport={{ once: true }}
-          initial={motions.fadeIn.initial}
-          whileInView={motions.fadeIn.whileInView}
-          transition={motions.transition({})}>
-          <Label className="text-base">
-            <Link href="/">PalestinianCauses.</Link>
-          </Label>
-        </MotionDiv>
-        <QueryProvider>
-          <AuthenticationButtons
-            serverState={Boolean(authentication && authentication.user)}
-          />
-        </QueryProvider>
+        <Label className="text-base">
+          <Link href="/">PalestinianCauses.</Link>
+        </Label>
+        <AuthenticationButtons />
       </nav>
       <div className="flex flex-col items-start justify-center">
         <h1 className="flex w-full max-w-2xl flex-wrap gap-x-3 text-6xl leading-none tracking-tight sm:text-8xl lg:text-7xl xl:text-8xl [@media_(max-height:48rem)_and_(max-width:27rem)]:text-5xl [@media_(max-height:48rem)_and_(min-width:40rem)]:text-7xl">
-          {[
-            "Passionate",
-            "and",
-            "Creative",
-            "Individuals",
-            "United",
-            "By",
-            "One",
-            "Mission.",
-          ].map((word, index) => (
-            <MotionSpan
-              key={word}
-              viewport={{ once: true }}
-              initial={motions.fadeIn.initial}
-              whileInView={motions.fadeIn.whileInView}
-              transition={motions.transition({ delay: index * 0.1 })}
-              className={cn("whitespace-break-spaces")}
-              style={{ willChange: "transform, opacity" }}>
-              {word}
-            </MotionSpan>
-          ))}
+          Passionate and Creative Individuals United By One Mission.
         </h1>
       </div>
       <footer className="flex w-full flex-col flex-wrap justify-start gap-5 xl:flex-row xl:items-end">
         <div className="flex flex-1 flex-col gap-1.5">
-          <MotionP
-            viewport={{ once: true }}
-            initial={motions.fadeIn.initial}
-            whileInView={motions.fadeIn.whileInView}
-            transition={motions.transition({})}
-            className="mr-auto text-sm leading-normal tracking-wide text-foreground">
+          <p className="mr-auto text-sm leading-normal tracking-wide text-foreground">
             &copy; 2025 PalestinianCauses LLC. All Rights Reserved.
-          </MotionP>
-          <QueryProvider>
-            <NotificationButton />
-          </QueryProvider>
+          </p>
+          <NotificationButton />
         </div>
         <ul className="flex flex-row flex-wrap items-center justify-start gap-5">
           {[
-            navigation.map(({ label, href }, index) => (
+            navigation.map(({ label, href }) => (
               <li key={href}>
-                <MotionDiv
-                  viewport={{ once: true }}
-                  initial={motions.fadeIn.initial}
-                  whileInView={motions.fadeIn.whileInView}
-                  transition={motions.transition({
-                    delay: index * 0.1,
-                  })}>
-                  <Button variant="link" className="p-0" asChild>
-                    <Link href={href}>{label}</Link>
-                  </Button>
-                </MotionDiv>
+                <Button variant="link" className="p-0" asChild>
+                  <Link href={href}>{label}</Link>
+                </Button>
               </li>
             )),
           ]}
