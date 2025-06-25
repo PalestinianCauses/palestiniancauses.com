a004fef1cd56e2d0d4b2f53f85646a450ab4a4ae
Shawqi Hatem <shawqicauses@outlook.com>
2025-06-25 12:08:30 +0200

    Fix: Enhance through fixing variants type in globals/navigation component

--- src/components/globals/navigation.tsx
+++ src/components/globals/navigation.tsx
@@ -1,9 +1,7 @@
-// REVIEWED - 08
+// REVIEWED - 09
 
-import { motions } from "@/lib/motion";
 import { HumansButFromGazaPageLink } from "@/lib/utils/strings";
 
-import { MotionSpan } from "./motion";
 import { NavigationLink } from "./navigation-link";
 
 export const navigation = [
@@ -47,22 +45,14 @@ export const navigation = [
 
 export const Navigation = function Navigation() {
   return (
-    <ul className="grid snap-y snap-mandatory snap-start auto-rows-[calc(100vh/5)] grid-rows-[repeat(5,_calc(100vh/5))] divide-y divide-muted overflow-y-scroll lg:auto-rows-[calc(100vh/4)] lg:grid-rows-[repeat(4,_calc(100vh/4))] [@media_(max-height:48rem)]:snap-none [@media_(max-height:48rem)]:auto-rows-[calc(48rem/4)] [@media_(max-height:48rem)]:grid-rows-[repeat(4,_calc(48rem/4))]">
-      {navigation.map(({ label, href, recent, coming }, index) => (
+    <ul className="no-scrollbar grid auto-rows-[calc(100vh/5)] grid-rows-[repeat(5,_calc(100vh/5))] divide-y divide-muted overflow-y-scroll lg:auto-rows-[calc(100vh/4)] lg:grid-rows-[repeat(4,_calc(100vh/4))] [@media_(max-height:48rem)]:auto-rows-[calc(48rem/4)] [@media_(max-height:48rem)]:grid-rows-[repeat(4,_calc(48rem/4))]">
+      {navigation.map(({ label, href, recent, coming }) => (
         <li
           key={href}
-          className="flex w-full snap-start items-center justify-start overflow-hidden">
-          <MotionSpan
-            viewport={{ once: true }}
-            initial={motions.fadeIn.initial}
-            whileInView={motions.fadeIn.whileInView}
-            whileHover="hover"
-            transition={motions.transition({ delay: index * 0.1 })}
-            className="h-full w-full">
-            <NavigationLink href={href} recent={recent} coming={coming}>
-              {label}
-            </NavigationLink>
-          </MotionSpan>
+          className="flex w-full items-center justify-start overflow-hidden">
+          <NavigationLink href={href} recent={recent} coming={coming}>
+            {label}
+          </NavigationLink>
         </li>
       ))}
     </ul>
