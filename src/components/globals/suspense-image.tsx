70965f94a126c4bc86b47fdd00c8f83cf1d96775
Shawqi Hatem <shawqicauses@outlook.com>
2025-06-25 23:17:48 +0200

    New: Enhance through adding suspense image component

---
+++ src/components/globals/suspense-image.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+// REVIEWED
+
+import Image, { ImageProps } from "next/image";
+import { ReactNode, useState } from "react";
+
+import { cn } from "@/lib/utils/styles";
+
+export const SuspenseImage = function SuspenseImage({
+  isLoadingElement,
+  containerClassName,
+  className,
+  onLoad,
+  ...props
+}: ImageProps & {
+  isLoadingElement?: ReactNode;
+  containerClassName?: string;
+}) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <div
+      className={cn(
+        "relative h-full w-full overflow-hidden",
+        containerClassName,
+      )}>
+      {isLoading && isLoadingElement}
+      <Image
+        {...props}
+        className={cn(
+          "transition-opacity duration-300 ease-in-out",
+          { "opacity-0": isLoading },
+          className,
+        )}
+        onLoad={(e) => {
+          setIsLoading(false);
+          if (onLoad) onLoad(e);
+        }}
+      />
+    </div>
+  );
+};
