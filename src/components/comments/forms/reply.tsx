df7d7ac52bf8702238d6c7b1214422167a00a38d
Shawqi Hatem <shawqicauses@outlook.com>
2025-06-26 11:35:56 +0200

    New: Enhance through adding reply form comment component

---
+++ src/components/comments/forms/reply.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+// REVIEWED
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { useComment } from "@/hooks/use-comment";
+import { useUser } from "@/hooks/use-user";
+import { messages } from "@/lib/messages";
+import {
+  createCommentSchema,
+  CreateCommentSchema,
+} from "@/lib/schemas/comment";
+import { Comment } from "@/payload-types";
+
+export const ReplyCommentForm = function ReplyCommentForm({
+  depth,
+  on,
+  parent,
+  onSuccess,
+}: {
+  depth: number;
+  onSuccess: () => void;
+} & Pick<Comment, "on" | "parent">) {
+  const { data: user } = useUser();
+  const { createComment } = useComment();
+
+  const form = useForm<CreateCommentSchema>({
+    mode: "onBlur",
+    defaultValues: { content: "" },
+    resolver: zodResolver(createCommentSchema),
+  });
+
+  if (!user) return null;
+
+  const handleSubmit = function handleSubmit(data: CreateCommentSchema) {
+    toast.loading(messages.actions.comment.pendingCreate, {
+      id: "create-comment",
+    });
+
+    createComment.mutate(
+      {
+        user,
+        on,
+        parent,
+        content: data.content,
+        status: "approved",
+      },
+      {
+        onSuccess: () => {
+          form.reset();
+          onSuccess();
+        },
+      },
+    );
+  };
+
+  return (
+    <div className="flex w-full flex-col items-start gap-5 p-2.5 md:flex-row">
+      <Avatar className="ring-1 ring-primary/20">
+        <AvatarFallback className="bg-muted/50">
+          {user.firstName ? user.firstName.charAt(0).toUpperCase() : "A"}
+        </AvatarFallback>
+      </Avatar>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="relative flex w-full flex-col gap-2.5">
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem className="w-full flex-1 space-y-0">
+                <FormLabel className="sr-only">Reply Content</FormLabel>
+                <FormControl>
+                  <Textarea {...field} rows={4} className="!mb-2 resize-none" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            disabled={createComment.isPending}
+            className="w-full self-end text-center md:w-max">
+            {createComment.isPending ? "Replying..." : "Reply"}
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
