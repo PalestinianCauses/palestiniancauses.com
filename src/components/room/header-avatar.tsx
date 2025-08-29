e2cc5c29a1b08e1d323e59df787a5b4459b6597e
Shawqi Hatem - Developer and Instructor <shawqicauses@outlook.com>
2025-08-29 20:35:47 +0200

    Update: Enhance through refactoring header avatar to its own component

---
+++ src/components/room/header-avatar.tsx
@@ -0,0 +1,42 @@
+// REVIEWED
+
+"use client";
+
+import { useState } from "react";
+
+import { getMediaAltText, getMediaSizeURL } from "@/lib/utils/media";
+import { Room } from "@/payload-types";
+
+import { SuspenseAvatar } from "../globals/suspense-avatar";
+import { Skeleton } from "../ui/skeleton";
+import { TooltipTrigger } from "../ui/tooltip";
+
+export const HeaderAvatar = function HeaderAvatar({
+  name,
+  photograph,
+}: Pick<Room, "name"> & Pick<Room["information"], "photograph">) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <TooltipTrigger asChild>
+      <SuspenseAvatar
+        className="z-10 h-36 w-36 border border-input md:h-48 md:w-48 lg:h-60 lg:w-60"
+        isLoading={isLoading}
+        isLoadingProps={{
+          className: "w-full aspect-square",
+          children: <Skeleton className="h-full w-full" />,
+        }}
+        avatarImageProps={{
+          src: getMediaSizeURL(photograph, "room-photograph") || undefined,
+          alt: getMediaAltText(photograph) || "Room's Photograph",
+          onLoad: () => setIsLoading(false),
+          onError: () => setIsLoading(false),
+        }}
+        avatarFallbackProps={{
+          className: "text-7xl font-light lg:text-9xl",
+          children: name.slice(0, 1).toUpperCase(),
+        }}
+      />
+    </TooltipTrigger>
+  );
+};
