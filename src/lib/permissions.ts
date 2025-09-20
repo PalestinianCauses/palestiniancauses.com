d3d28f3ba06b0352758fd3d6fd682d10251927d9
Shawqi Hatem - Developer and Instructor <shawqicauses@outlook.com>
2025-09-20 14:49:27 +0200

    New: Enhance through adding permissions utilities file

---
+++ src/lib/permissions.ts
@@ -0,0 +1,132 @@
+// REVIEWED
+
+import { Permission, Role, User } from "@/payload-types";
+
+import { isConditionsObject, isDefined, isObject } from "./types/guards";
+
+export const hasConditions = function hasConditions(
+  permissionConditions: Permission["conditions"],
+  requestConditions: Permission["conditions"],
+): boolean {
+  if (!isDefined(permissionConditions) || !isDefined(requestConditions))
+    return true;
+
+  if (
+    !isConditionsObject(permissionConditions) ||
+    !isConditionsObject(requestConditions)
+  )
+    return true;
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const [key, value] of Object.entries(permissionConditions))
+    if (!(key in requestConditions) || requestConditions[key] !== value)
+      return false;
+
+  return true;
+};
+
+export const hasPermission = async function hasPermission(
+  user: User | null,
+  permission: Permission,
+): Promise<boolean> {
+  if (!user || !user.roles) return false;
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const role of user.roles) {
+    if (isObject(role) && isDefined(role.permissions)) {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const permissionUser of role.permissions) {
+        if (
+          isObject(permissionUser) &&
+          permissionUser.resource === permission.resource &&
+          permissionUser.action === permission.action
+        ) {
+          if (permissionUser.conditions && permission.conditions) {
+            const hasCondition = hasConditions(
+              permissionUser.conditions,
+              permission.conditions,
+            );
+
+            // eslint-disable-next-line no-continue
+            if (!hasCondition) continue;
+          }
+
+          return true;
+        }
+      }
+    }
+  }
+
+  return false;
+};
+
+export const hasAnyPermission = async function hasAnyPermission(
+  user: User | null,
+  permissions: Permission[],
+): Promise<boolean> {
+  if (!user || !user.roles) return false;
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const permission of permissions)
+    if (
+      // eslint-disable-next-line no-await-in-loop
+      await hasPermission(user, permission)
+    )
+      return true;
+
+  return false;
+};
+
+export const hasEveryPermissions = async function hasEveryPermissions(
+  user: User | null,
+  permissions: Permission[],
+): Promise<boolean> {
+  if (!user || !user.roles) return false;
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const permission of permissions)
+    if (
+      // eslint-disable-next-line no-await-in-loop
+      !(await hasPermission(user, permission))
+    )
+      return false;
+
+  return true;
+};
+
+export const hasRole = function hasRole(
+  user: User | null,
+  role: string,
+): boolean {
+  if (!user || !user.roles) return false;
+  return user.roles.some((r) => isObject(r) && r.name === role);
+};
+
+export const hasAnyRole = function hasAnyRole(
+  user: User | null,
+  roles: string[],
+): boolean {
+  if (!user || !user.roles) return false;
+  return user.roles.some((role) => isObject(role) && roles.includes(role.name));
+};
+
+export const getHighestPriorityRole = function getHighestPriorityRole(
+  user: User | null,
+): Role | null {
+  if (!user || !user.roles || user.roles.length === 0) return null;
+
+  let highestPriorityRole: Role | null = null;
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const role of user.roles)
+    if (
+      isObject(role) &&
+      isDefined(role.priority) &&
+      (highestPriorityRole === null ||
+        (isDefined(highestPriorityRole.priority) &&
+          role.priority > highestPriorityRole.priority))
+    )
+      highestPriorityRole = role;
+
+  return highestPriorityRole;
+};
