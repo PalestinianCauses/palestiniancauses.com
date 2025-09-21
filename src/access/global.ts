b69fb941924458c58ba44a31ca7b9e94a9d7652d
Shawqi Hatem - Developer and Instructor <shawqicauses@outlook.com>
2025-09-21 04:09:58 +0200

    New: Enhance through adding new permissions access functions

--- src/access/global.ts
+++ src/access/global.ts
@@ -1,5 +1,14 @@
-// REVIEWED - 02
+// REVIEWED - 03
+
 import { Access, FieldAccess } from "payload";
+
+import {
+  hasAnyPermission,
+  hasEveryPermissions,
+  hasPermission,
+  hasRole,
+} from "@/lib/permissions";
+import { Permission } from "@/payload-types";
 
 export const isAuthenticated: Access = function isAuthenticated({
   req: { user },
@@ -8,13 +17,13 @@ export const isAuthenticated: Access = function isAuthenticated({
 };
 
 export const isAdmin: Access = function isAdmin({ req }) {
-  return isAuthenticated({ req }) && req.user?.role === "admin";
+  return isAuthenticated({ req }) && hasRole(req.user, "admin");
 };
 
 export const isAdminOrSystemUser: Access = function isAdminOrSystemUser({
   req,
 }) {
-  return isAdmin({ req }) || req.user?.role === "system-user";
+  return isAdmin({ req }) || hasRole(req.user, "system-user");
 };
 
 export const isAdminOrSelf: Access = function isAdminOrSelf({ req }) {
@@ -27,10 +36,59 @@ export const isAdminOrSystemUserOrSelf: Access =
   };
 
 export const isAdminField: FieldAccess = function isAdminField({ req }) {
-  return isAuthenticated({ req }) && req.user?.role === "admin";
+  return isAuthenticated({ req }) && hasRole(req.user, "admin");
 };
 
 export const isAdminOrSystemUserField: FieldAccess =
   function isAdminOrSystemUserField({ req }) {
-    return isAdminField({ req }) || req.user?.role === "system-user";
+    return isAdminField({ req }) || hasRole(req.user, "system-user");
   };
+
+export const hasPermissionAccess = (permission: Permission) => {
+  const hasPermissionAccessCB: Access = async function hasPermissionAccessCB({
+    req,
+  }) {
+    if (!isAuthenticated({ req })) return false;
+    return hasPermission(req.user, permission);
+  };
+
+  return hasPermissionAccessCB;
+};
+
+export const hasAnyPermissionAccess = (permissions: Permission[]) => {
+  const hasAnyPermissionAccessCB: Access =
+    async function hasAnyPermissionAccessCB({ req }) {
+      if (!isAuthenticated({ req })) return false;
+      return hasAnyPermission(req.user, permissions);
+    };
+
+  return hasAnyPermissionAccessCB;
+};
+
+export const hasEveryPermissionsAccess = (permissions: Permission[]) => {
+  const hasEveryPermissionsAccessCB: Access =
+    async function hasEveryPermissionsAccessCB({ req }) {
+      if (!isAuthenticated({ req })) return false;
+      return hasEveryPermissions(req.user, permissions);
+    };
+
+  return hasEveryPermissionsAccessCB;
+};
+
+export const hasRoleAccess = (role: string) => {
+  const hasRoleAccessCB: Access = function hasRoleAccessCB({ req }) {
+    return isAuthenticated({ req }) && hasRole(req.user, role);
+  };
+
+  return hasRoleAccessCB;
+};
+
+export const hasAnyRoleAccess = (roles: string[]) => {
+  const hasAnyRoleAccessCB: Access = function hasAnyRoleAccessCB({ req }) {
+    return (
+      isAuthenticated({ req }) && roles.some((role) => hasRole(req.user, role))
+    );
+  };
+
+  return hasAnyRoleAccessCB;
+};
