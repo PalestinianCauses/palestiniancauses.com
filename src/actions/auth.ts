9cb0bed0b0803d7402bff4c31fbf7b1fec7aa097
Shawqi Hatem - Developer and Instructor <shawqicauses@outlook.com>
2025-10-11 14:33:42 +0200

    Update: Enhance through updating authentication server action to return user with populated roles and permissions

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,12 +1,13 @@
 "use server";
 
-// REVIEWED - 13
+// REVIEWED - 14
 
 import { headers } from "next/headers";
 
 import { messages } from "@/lib/messages";
 import { actionSafeExecute } from "@/lib/network";
 import { payload } from "@/lib/payload";
+import { isDefined } from "@/lib/types/guards";
 
 export const getAuthentication = async function getAuthentication() {
   const response = await actionSafeExecute(
@@ -14,7 +15,25 @@ export const getAuthentication = async function getAuthentication() {
     messages.actions.user.serverError,
   );
 
-  if (!response.data || response.error) return null;
+  if (
+    !response.data ||
+    response.error ||
+    !isDefined(response.data.user) ||
+    !isDefined(response.data.user.id)
+  )
+    return null;
 
-  return response.data;
+  // Get user with populated roles and permissions
+  const userResponse = await actionSafeExecute(
+    payload.findByID({
+      collection: "users",
+      id: response.data.user.id,
+      depth: 2, // Populate roles and their permissions
+    }),
+    messages.actions.user.serverError,
+  );
+
+  if (!userResponse.data || userResponse.error) return null;
+
+  return userResponse.data;
 };
