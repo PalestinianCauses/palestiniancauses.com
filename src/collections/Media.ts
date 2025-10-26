05cf7f15510ed7115a3f804bb8fd41e565f72173
Shawqi Hatem - Developer and Instructor <shawqicauses@outlook.com>
2025-10-26 03:06:43 +0200

    Update: Enhance through updating media collection’s access controls

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,15 +1,15 @@
-// REVIEWED - 05
+// REVIEWED - 06
 import type { CollectionConfig } from "payload";
 
-import { isAdmin } from "@/access/global";
+import { hasPermissionAccess } from "@/access/global";
 
 export const Media: CollectionConfig = {
   slug: "media",
   access: {
-    read: () => true,
-    create: isAdmin,
-    update: isAdmin,
-    delete: isAdmin,
+    read: hasPermissionAccess({ resource: "media", action: "read" }),
+    create: hasPermissionAccess({ resource: "media", action: "create" }),
+    update: hasPermissionAccess({ resource: "media", action: "update" }),
+    delete: hasPermissionAccess({ resource: "media", action: "delete" }),
   },
   admin: {
     group: "Database",
