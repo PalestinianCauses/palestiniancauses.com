3e8b7b4c54e0ab1b0f61dfe875bf50370796c981
Shawqi Hatem - Developer and Instructor <shawqicauses@outlook.com>
2025-11-17 17:36:44 +0200

    New: Enhance through adding achievements notification collection

---
+++ src/collections/AchievementNotifications.ts
@@ -0,0 +1,76 @@
+// REVIEWED
+
+import { CollectionConfig } from "payload";
+
+import { hasPermissionAccess, isSelf } from "@/access/global";
+import { hasPermission } from "@/lib/permissions";
+import { User } from "@/payload-types";
+
+export const AchievementNotifications: CollectionConfig = {
+  slug: "achievement-notifications",
+  access: {
+    admin: ({ req }) =>
+      hasPermission(req.user, {
+        resource: "achievement-notifications",
+        action: "manage",
+      }),
+    create: hasPermissionAccess({
+      resource: "achievement-notifications",
+      action: "create",
+    }),
+    read: ({ req }) =>
+      hasPermissionAccess({
+        resource: "achievement-notifications",
+        action: "read",
+      })({ req }) || isSelf("user")({ req }),
+    update: hasPermissionAccess({
+      resource: "achievement-notifications",
+      action: "update",
+    }),
+    delete: hasPermissionAccess({
+      resource: "achievement-notifications",
+      action: "delete",
+    }),
+  },
+  admin: {
+    hidden: ({ user }) =>
+      !hasPermission(user as unknown as User, {
+        resource: "achievement-notifications",
+        action: "manage",
+      }),
+    group: "Database",
+    defaultColumns: ["user", "achievement", "notified", "notifiedAt"],
+    useAsTitle: "id",
+  },
+  fields: [
+    {
+      name: "user",
+      label: "User",
+      type: "relationship",
+      relationTo: "users",
+      required: true,
+      index: true,
+    },
+    {
+      name: "achievement",
+      label: "Achievement",
+      type: "text",
+      required: true,
+      index: true,
+    },
+    {
+      name: "notified",
+      label: "Notified",
+      type: "checkbox",
+      defaultValue: false,
+      required: true,
+    },
+    {
+      admin: { date: { pickerAppearance: "dayAndTime" } },
+      name: "notifiedAt",
+      label: "Notified At",
+      type: "date",
+      required: false,
+    },
+  ],
+};
