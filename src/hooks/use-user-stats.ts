3e3f3d240ac38512700e594968a1104012186ae2
Shawqi Hatem - Developer and Instructor <shawqicauses@outlook.com>
2025-11-21 00:36:29 +0200

    New: Enhance through adding use user stats hook

---
+++ src/hooks/use-user-stats.ts
@@ -0,0 +1,21 @@
+"use client";
+
+// REVIEWED
+
+import { useQuery } from "@tanstack/react-query";
+
+import { getUserStats } from "@/actions/user-stats";
+
+export const useUserStats = function useUserStats() {
+  const query = useQuery({
+    queryKey: ["user-stats"],
+    queryFn: async () => {
+      const response = await getUserStats();
+      return response;
+    },
+    staleTime: 60 * 60 * 1000, // 60 minutes - stats don't change frequently
+    gcTime: 24 * 60 * 60 * 1000, // 24 hours
+  });
+
+  return query;
+};
