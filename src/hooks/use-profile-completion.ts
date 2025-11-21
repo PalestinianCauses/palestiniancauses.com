a9b28ec313e30406f3443feb71865b9d409f2035
Shawqi Hatem - Developer and Instructor <shawqicauses@outlook.com>
2025-11-21 02:18:48 +0200

    New: Enhance through adding use profile completion hook

---
+++ src/hooks/use-profile-completion.ts
@@ -0,0 +1,62 @@
+"use client";
+
+// REVIEWED
+
+import { useMemo } from "react";
+
+import { useUser } from "./use-user";
+
+export const useProfileCompletion = function useProfileCompletion() {
+  const { data: user } = useUser();
+
+  const { completion, suggestions } = useMemo(() => {
+    if (!user) return { completion: 0, suggestions: [] };
+
+    const checks = [
+      {
+        name: "First Name",
+        completed: Boolean(user.firstName),
+        suggestion: "Introduce yourself by sharing your first name.",
+        link: "/profile/settings",
+      },
+      {
+        name: "Last Name",
+        completed: Boolean(user.lastName),
+        suggestion: "Provide your last name to help others recognize you.",
+        link: "/profile/settings",
+      },
+      {
+        name: "Bio",
+        completed: Boolean(user.bio),
+        suggestion:
+          "Enrich your profile with a brief biography so our community can get to know you.",
+        link: "/profile/settings",
+      },
+      {
+        name: "Avatar",
+        completed: Boolean(user.avatar),
+        suggestion: "Make a strong impression by uploading a profile picture.",
+        link: "/profile/settings",
+      },
+      {
+        name: "Email Verified",
+        // eslint-disable-next-line no-underscore-dangle
+        completed: Boolean(user._verified),
+        suggestion:
+          "Enhance your account security by verifying your email address.",
+        link: "/profile/settings",
+      },
+    ];
+
+    const total = checks.length;
+
+    const completed = checks.filter((check) => check.completed).length;
+    const percentageCompletion = Math.round((completed / total) * 100);
+
+    const arraySuggestions = checks.filter((check) => !check.completed);
+
+    return { completion: percentageCompletion, suggestions: arraySuggestions };
+  }, [user]);
+
+  return { completion, suggestions };
+};
